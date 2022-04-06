1dfeee28a1fb1bd0641954dcc67631c75d1a278b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-06 19:42:22 +0530

    Maximum and Minimum number In Array

---
+++ Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array.js
@@ -0,0 +1,14 @@
+// let numbers =  [9,5,78,5,4,3343,6,8,7,56,1,5,6,86,6]
+
+// console.log(Math.max(...numbers))
+
+
+// console.log(Math.min(...numbers))
+
+
+function sum (a,b,c){
+    console.log(a+b+c)
+}
+
+let arrVal = [1,2,3]
+sum.apply(null, arrVal)
