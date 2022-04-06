7a9dd6c938dab34401b08f33092ad3f9d622572b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-06 19:40:39 +0530

    arrays with functions

---
+++ Lecture_009_Arrays/009_02_Arrays_With_Fuctions.js
@@ -0,0 +1,12 @@
+// May be this is only for c++. not for javascript. 
+
+
+let printArray = (arr) => {
+    for (i = 0; i <= arr.length-1; i++){
+        console.log(arr[i])
+    }
+    console.log('Printing Done')
+}
+
+
+printArray(12)
