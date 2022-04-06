133833e2b5f8fbbba054390969f182a0da4f3e87
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-06 19:41:15 +0530

    Different types of Arrays

---
+++ Lecture_009_Arrays/009_03_Different_types_of_Arrays.js
@@ -0,0 +1,23 @@
+let Array = ['sun', 'moon', 'Star']
+console.log(Array[2])
+
+// Output:
+// Star
+
+// // ---------------------------------------
+
+
+let printArray  = (arr) => {
+    for(let i = 0; i<= arr.length-1; i++){
+        console.log(arr[i])
+    }
+}
+
+let Array2 = ['Sun', 'Moon', 'Stars']
+printArray(Array2)
+
+// Output:
+// Star
+// Sun
+// Moon
+// Stars
