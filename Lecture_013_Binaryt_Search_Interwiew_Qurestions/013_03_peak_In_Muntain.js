9d02752e8728f7363151ecbfce039943498bb086
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-12 08:38:36 +0530

    013_03_peak_In_Muntain

---
+++ Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_03_peak_In_Muntain.js
@@ -0,0 +1,34 @@
+// peak in mountain array // leetcode 
+
+
+
+let peakIndex = (arr) => {
+    let s = 0 
+    let e = arr.length-1
+
+    let mid = s + Math.floor((e-s) / 2)
+
+    while (s < e){
+        if (arr[mid] < arr[mid + 1]){
+           s = mid + 1
+        }else{
+            e = mid
+        }
+        mid = s + Math.floor((e - s) / 2)
+    }
+    return s
+}
+
+
+let test1 = [3,4,5,6,8,9,5,4,1]
+let test2 = [3,4,5,1]
+let test3 = [0,8,10,5,2]
+
+let check1 = peakIndex(test1)
+let check2 = peakIndex(test2)
+let check3 = peakIndex(test3)
+
+
+console.log(check1)
+console.log(check2)
+console.log(check3)
