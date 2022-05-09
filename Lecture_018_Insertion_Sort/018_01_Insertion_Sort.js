f91b3e2a93a104ede8d8a712b31025a9632c659e
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-09 19:00:06 +0530

    018_01_Insertion_Sort

---
+++ Lecture_018_Insertion_Sort/018_01_Insertion_Sort.js
@@ -0,0 +1,18 @@
+const Insertion = (arr) => {
+     for (let i = 0; i <arr.length; i++){
+         for (let j = i+1; j <arr.length; j++){
+             if (arr[i] > arr[j]){
+                 temp = arr[i]
+                 arr[i] = arr[j] 
+                 arr[j] = temp
+             }
+         }
+     }
+     return arr
+}
+
+
+const arr1 = [2,6,4,8,10,16,14]
+
+const testCase1 = Insertion(arr1)
+console.log(testCase1)
