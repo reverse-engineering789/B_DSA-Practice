b1206f498984d1d218b6a71a27bdfe8cbfdc3643
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-09 18:16:21 +0530

    016_03_Selection_Sort-3.js

---
+++ Lecture_016_Selection_Sort/016_03_Selection_Sort-3.js
@@ -0,0 +1,23 @@
+//  function SelectionSort (arr) {
+//    for (let i = 0; i < arr.length; i++){
+//        let lowest = i
+//        for (let j = 0; j < arr.length; j++ ){
+//               if (arr[j] > arr[lowest]){
+//                   lowest = j
+//               }
+//        }
+//        if (i !== lowest){
+//            let temp = arr[i]
+//            arr[i] = arr[lowest]
+//            arr[lowest] = temp
+//        }
+//    }
+//    return arr
+// }
+
+
+// console.log(SelectionSort([0,2,34,22,10,19,17]))
+
+
+
+
