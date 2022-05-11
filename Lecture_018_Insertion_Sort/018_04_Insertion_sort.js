320f77166226f6c587dcbcc199f92880c71dfd9b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-11 10:38:09 +0530

    Insertion_sort

---
+++ Lecture_018_Insertion_Sort/018_04_Insertion_sort.js
@@ -0,0 +1,60 @@
+
+// function insertionSort (arr) {
+//     let currentVal
+//     let j 
+//     for (let i = 1; i < arr.length; i++){
+//         currentVal = arr[i]
+//         for ( j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+//            arr[j+1]  = arr[j]
+//         }
+//         arr[j+1] = currentVal
+//     }
+//     return arr
+// }
+
+// console.log(insertionSort([2,1,9,76,4]))
+
+
+
+// // ----------------------------------------------------
+
+
+
+// function bubbleSort (arr) {
+//     let currentVal
+//     let j
+//     for (let i = 0; i < arr.length; i++){
+//          arr[i]  = currentVal
+//         for (j = i - 1; j >= 0 && j > currentVal; j--){
+//             arr[j+1] = arr[j]
+//         }
+//         arr[j+1] = currentVal
+//     }
+//     return arr
+// }
+
+// console.log(bubbleSort([2,1,9,76,4]))
+
+
+
+// //--------------------------------------------------------------
+
+
+// const BubbbleSort = (arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//            arr[i] = currentVal
+//             for (let j = i - 1; j >= 0 && j > currentVal; j--){
+//                 arr[j+1] = arr[j]
+//             }
+//                 arr[j+1] = currentVal
+// }
+// return arr
+// }
+
+// console.log(BubbbleSort([2,1,9,76,4])) 
+
+
+// //-------------------------------------------------------------
+
+
+
