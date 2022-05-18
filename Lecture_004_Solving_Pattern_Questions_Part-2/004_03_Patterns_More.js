014229b33a3919ce1ad86dcb6812bc35a473acdd
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-18 13:20:05 +0530

    004_03_Patterns_More.js

---
+++ Lecture_004_Solving_Pattern_Questions_Part-2/004_03_Patterns_More.js
@@ -0,0 +1,32 @@
+const n = 4
+
+let count = 1
+for (let i = 0; i < n; i++){
+    for (let j = 0; j < n; j++){
+        console.log(count)
+        count++
+    }
+    console.log(' ')
+}
+
+
+// Output:
+// 1
+// 2
+// 3
+// 4
+
+// 5
+// 6
+// 7
+// 8
+
+// 9
+// 10
+// 11
+// 12
+
+// 13
+// 14
+// 15
+// 16
