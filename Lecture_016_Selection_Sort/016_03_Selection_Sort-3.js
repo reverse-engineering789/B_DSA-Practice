62476d9d426bcae88733e352a89e021ddd0e6070
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-19 18:21:31 +0530

    Lecture_016_Selection_Sort/016_03_Selection_Sort-3.js

--- Lecture_016_Selection_Sort/016_03_Selection_Sort-3.js
+++ Lecture_016_Selection_Sort/016_03_Selection_Sort-3.js
@@ -18,6 +18,26 @@
 
 // console.log(SelectionSort([0,2,34,22,10,19,17]))
 
+// // ---------------------------------------------
 
+
+
+
+function SelectionSort (arr) {
+    for (let i = 0; i < arr.length; i++){
+        let lowest = i;
+        for (let j = i + 1; j < arr.length; j++){
+            if (arr[j] < arr[lowest]){
+                lowest = j
+            }
+        }
+        if (i !== lowest){
+            let temp = arr[i]
+            arr[i] = arr[lowest]
+            arr[lowest] = temp
+        }
+    }
+    return arr
+}
 
 
