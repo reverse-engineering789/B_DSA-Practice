f5af561fd52f8745ae06eefad25110d4ec882909
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-19 18:21:06 +0530

    implementation.js

--- Lecture_009_Arrays/009_01_Arrays_Implimentation.js
+++ Lecture_009_Arrays/009_01_Arrays_Implimentation.js
@@ -6,7 +6,7 @@ console.log('The value of index 2 is: ' + test[2])
 // Output:
 // The value of index 1 is: 4
 // The value of index 2 is: 6
-
+ 
 
 for(let i = 0; i <= test.length-1; i++){
    console.log(test[i])    
