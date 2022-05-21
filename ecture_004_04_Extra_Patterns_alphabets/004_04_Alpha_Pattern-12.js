566282bfe3010a8349dce82cb821a91216eae8e3
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-22 00:48:44 +0530

    004_04_Alpha_Pattern-12.js

---
+++ ecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-12.js
@@ -0,0 +1,29 @@
+let n = 5;
+let string = "";
+// Pyramid
+for (let i = 1; i <= n; i++) {
+  for (let k = 0; k < i; k++) {
+    string += String.fromCharCode(k + 65);
+  }
+  string += "\n";
+}
+// Reverse Pyramid
+for (let i = 1; i <= n - 1; i++) {
+  for (let k = 0; k < n - i; k++) {
+    string += String.fromCharCode(k + 65);
+  }
+  string += "\n";
+}
+console.log(string);
+
+
+// Output:
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
+// ABCD
+// ABC
+// AB
+// A
