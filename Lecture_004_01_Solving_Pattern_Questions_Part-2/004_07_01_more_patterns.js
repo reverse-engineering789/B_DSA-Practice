4dc2b8e0dd8fa9cb3f6fb4cae26b7f2f0db6462e
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-27 21:59:26 +0530

    004_07_01_more_patterns.js

---
+++ Lecture_004_01_Solving_Pattern_Questions_Part-2/004_07_01_more_patterns.js
@@ -0,0 +1,23 @@
+const n = 4
+
+for (let i = 1; i <= n; i++){
+    for (let j = 1; j <= i; j++){
+          console.log(i)
+        
+    }
+    console.log(' ')
+}
+
+// 1
+ 
+// 2
+// 2
+
+// 3
+// 3
+// 3
+
+// 4
+// 4
+// 4
+// 4
