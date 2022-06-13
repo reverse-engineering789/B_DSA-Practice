edae2627bec05afbb9cdcd2df079e8d47e70b7a8
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-13 21:05:27 +0530

    004_07_03_Alpha_Pattern-3.js

---
+++ Lecture_004_07_Patterns_Practice_Star/004_07_03_Alpha_Pattern-3.js
@@ -0,0 +1,21 @@
+const n = 5 
+
+let string = ''
+
+let count = 1
+
+for (let i = 1; i <= n; i++){
+    for (let j = 1; j <= i; j++){
+        string = string + String.fromCharCode(count + 64)
+        count++
+    }    
+    string = string + '\n'
+}
+console.log(string)
+
+// Output:
+// A
+// BC
+// DEF
+// GHIJ
+// KLMNO
