a18b7668dd5555221c3d5d30c86108a8885a15c8
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-13 21:02:59 +0530

    Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-5.js

--- Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-5.js
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-5.js
@@ -14,3 +14,21 @@ console.log(string);
 // EDC
 // ED
 // E
+
+// -----------------------------------
+// const n = 5 
+// string = '';
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j <= n-i+1; j++){
+//        string = string + String.fromCharCode((n+1-j)+64)
+//     }
+//     string = string + '\n'
+// }
+// console.log(string)
+
+// // Output:
+// // EDCBA
+// // EDCB
+// // EDC
+// // ED
+// // E
