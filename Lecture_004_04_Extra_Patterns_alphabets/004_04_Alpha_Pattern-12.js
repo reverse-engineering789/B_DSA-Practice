8a1dd022af65779bbd0763a2416d1e8086bf85a3
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-18 08:40:09 +0530

    Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-12.js

--- Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-12.js
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-12.js
@@ -27,3 +27,34 @@ console.log(string);
 // ABC
 // AB
 // A
+
+// my code: 
+// const n = 5 
+// string = ''
+
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j <= i; j++){
+//          string = string + String.fromCharCode(j + 64)
+//     }
+//     string = string + '\n'
+// }
+
+// for (let i = 1; i < n; i++){
+//      for (let k = 1; k <= (n-i); k++){
+//          string = string + String.fromCharCode(k + 64)
+//      }
+//      string = string + '\n'
+// }
+// console.log(string)
+
+
+// // Output:
+// // A
+// // AB
+// // ABC
+// // ABCD
+// // ABCDE
+// // ABCD
+// // ABC
+// // AB
+// // A
