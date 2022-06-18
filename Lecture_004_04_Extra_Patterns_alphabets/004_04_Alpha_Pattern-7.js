ecd33073bb98d03cf6f5c5852b7ef1f705accc06
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-18 08:39:04 +0530

    Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-7.js

--- Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-7.js
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-7.js
@@ -21,3 +21,20 @@ console.log(string);
 //   ABCDE
 //  ABCDEFG
 // ABCDEFGHI
+
+
+// const n = 5 
+
+// string = ''
+
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j<=(n-i+1); j++ ){
+//         string = string + ' '
+//     }
+//     for (let k = 1; k <= ((2*i)-1); k++){
+//         string = string + String.fromCharCode((k) + 64)
+//     }
+//     string = string + '\n'
+// }
+
+// console.log(string)
