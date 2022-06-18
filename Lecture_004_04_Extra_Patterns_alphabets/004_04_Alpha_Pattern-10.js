de2f503f8bd78be3a5fcfd0b382e3166e5dcc950
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-18 08:36:53 +0530

    Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-10.js

--- Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-10.js
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-10.js
@@ -31,3 +31,41 @@ console.log(string);
 //   ABCDE
 //    ABC
 //     A
+
+// My code:
+// const n = 5 
+// string = ''
+
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j <= n-i; j++){
+//        string = string + ' '
+//     }
+//     for (let k = 1; k <= (2*i)-1; k++ ){
+//         string = string + String.fromCharCode(k + 64)
+//     }
+//     string = string + '\n'  
+// }
+
+
+// for (let i = 1; i < n; i++){
+//     for (let j = 1; j <= i; j++){
+//         string = string + ' '
+//     }
+//     for (let k = 1; k <= (2*(n-i))-1; k++){
+//         string = string + String.fromCharCode(k + 64)
+//     }
+//     string = string + '\n'
+// }
+// console.log(string)
+
+
+// // Output:
+// //     A
+// //    ABC
+// //   ABCDE
+// //  ABCDEFG
+// // ABCDEFGHI
+// //  ABCDEFG
+// //   ABCDE
+// //    ABC
+// //     A
