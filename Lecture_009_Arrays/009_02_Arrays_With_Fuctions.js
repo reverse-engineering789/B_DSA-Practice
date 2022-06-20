2d91de9c587f612016bc6cd47e4105f1e6ef7f8a
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-21 01:10:03 +0530

    Lecture_009_Arrays/009_02_Arrays_With_Fuctions.js

--- Lecture_009_Arrays/009_02_Arrays_With_Fuctions.js
+++ Lecture_009_Arrays/009_02_Arrays_With_Fuctions.js
@@ -1,12 +1,33 @@
-// May be this is only for c++. not for javascript. 
+// // May be this is only for c++. not for javascript. 
 
 
-let printArray = (arr) => {
-    for (i = 0; i <= arr.length-1; i++){
-        console.log(arr[i])
+// let printArray = (arr) => {
+//     for (i = 0; i <= arr.length-1; i++){
+//         console.log(arr[i])
+//     }
+//     console.log('Printing Done')
+// }
+
+
+// printArray(12)
+
+// // Output:
+// // 009_02_Arrays_With_Fuctions
+
+
+// // --------------------------------------------------
+
+
+function printArray(arr){
+    str = ''
+    for (let i = 0; i <= arr; i++){
+        str = str + arr[i] + ' '
     }
-    console.log('Printing Done')
+    console.log('Printing is Done - 2nd Code')
 }
 
 
-printArray(12)
+printArray(10)
+
+// Output:
+// Printing is Done - 2nd Code
