8d063eeca188a7f87ac46c4d51ced2c4c396cf9d
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-20 22:14:56 +0530

    Lecture_008_Switch_Cases_and_functions/008_02_Claculator_Switch_Statement.js

--- Lecture_008_Switch_Cases_and_functions/008_02_Claculator_Switch_Statement.js
+++ Lecture_008_Switch_Cases_and_functions/008_02_Claculator_Switch_Statement.js
@@ -1,28 +1,57 @@
-let FirstNumber = 2
-let Operation = '*'
-let SecondNumber = 4
+// let FirstNumber = 2
+// let Operation = '*'
+// let SecondNumber = 4
 
-switch(Operation){
-    case '+':
-        console.log('The sum of two numbers is: ' + FirstNumber + SecondNumber)
-       break;
+// switch(Operation){
+//     case '+':
+//         console.log('The sum of two numbers is: ' + FirstNumber + SecondNumber)
+//        break;
     
+//     case '-':
+//         console.log('The answer is: '+  FirstNumber - SecondNumber)
+//        break;
+    
+//        case '*':
+//         console.log('The answer is: '+  FirstNumber * SecondNumber)
+//        break;
+    
+//        case '/':
+//         console.log('The answer is: '+  FirstNumber / SecondNumber)
+//        break;
+
+//        default:
+//            console.log('Invalid Input')
+//     }
+
+
+// // Output:
+// // The answer is: 8
+
+
+
+// Calculator:
+
+const fNum = 1
+const sNum = 2
+const sign = '/'
+
+switch(sign) {
+   case '+':
+        console.log(`result: ${fNum + sNum}`)
+        break
     case '-':
-        console.log('The answer is: '+  FirstNumber - SecondNumber)
-       break;
-    
-       case '*':
-        console.log('The answer is: '+  FirstNumber * SecondNumber)
-       break;
-    
-       case '/':
-        console.log('The answer is: '+  FirstNumber / SecondNumber)
-       break;
-
-       default:
-           console.log('Invalid Input')
-    }
+        console.log(`result: ${fNum - sNum}`)
+        break
+   case '*':
+        console.log(`result: ${fNum * sNum}`)
+        break
+   case '/':
+        console.log(`result: ${(fNum / sNum)}`)
+        break
+    default:
+        console.log('Invalid Input')
+}  
 
 
 // Output:
-// The answer is: 8
+// 0.5
