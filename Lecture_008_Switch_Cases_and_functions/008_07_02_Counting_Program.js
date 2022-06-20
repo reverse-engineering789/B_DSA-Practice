3108612115b188df88b7e49576caf28cf1036d0f
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-20 22:09:27 +0530

    Lecture_008_Switch_Cases_and_functions/008_07_02_Counting_Program.js

---
+++ Lecture_008_Switch_Cases_and_functions/008_07_02_Counting_Program.js
@@ -0,0 +1,32 @@
+// const n = 10
+
+// let string = ''
+
+
+// for (let i = 1; i <= n; i++ ){
+//      string = string + i + ' '
+// }
+
+// console.log(string)
+
+// // Output:
+// // 1 2 3 4 5 6 7 8 9 10 
+
+// //----------------------------------------
+
+
+
+
+// const n = 10
+
+// let string = ''
+
+
+// for (let i = 1; i <= n; i++ ){
+//      string += i + ' '
+// }
+
+// console.log(string)
+
+// // Output:
+// // 1 2 3 4 5 6 7 8 9 10 
