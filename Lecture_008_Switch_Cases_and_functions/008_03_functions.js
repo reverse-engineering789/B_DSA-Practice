ac3c725184b198602cbad8a1c5dede57b64216bc
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-20 22:15:54 +0530

    Lecture_008_Switch_Cases_and_functions/008_03_functions.js

--- Lecture_008_Switch_Cases_and_functions/008_03_functions.js
+++ Lecture_008_Switch_Cases_and_functions/008_03_functions.js
@@ -1,38 +1,85 @@
 
-let Power = (a, b) => {
-    let findPower = 1
-   for ( let i = 1; i <= b; i++){
-          findPower = findPower * a
-}
-console.log('The power of the given number: ' + findPower)
-   }
+// let Power = (a, b) => {
+//     let findPower = 1
+//    for ( let i = 1; i <= b; i++){
+//           findPower = findPower * a
+// }
+// console.log('The power of the given number: ' + findPower)
+//    }
 
-let test1 = Power(2,4)
-let test2 = Power(2,6)
-
-
-// Output:
-// The power of the given number: 16
-// The power of the given number: 64
-
-//-------------------------------------------------
+// let test1 = Power(2,4)
+// let test2 = Power(2,6)
 
 
+// // Output:
+// // The power of the given number: 16
+// // The power of the given number: 64
+
+// //-------------------------------------------------
 
 
-let power = (a,b) => {
-    let ans = 1
-for (let i = 1; i <= b; i++){
-       ans = ans * a 
-}
-return ans
-}
+
+
+// let power = (a,b) => {
+//     let ans = 1
+// for (let i = 1; i <= b; i++){
+//        ans = ans * a 
+// }
+// return ans
+// }
     
 
 
-let moon = power(4,6)
-console.log('the answer is: ' + moon)
+// let moon = power(4,6)
+// console.log('the answer is: ' + moon)
 
 
-// Output:
-// the answer is: 4096
+// // Output:
+// // the answer is: 4096]
+
+let powerfind = (num, pow2) => {
+    let result = 1
+    for (let i = 1; i <= pow2; i++){
+            result = result * num
+    }
+     return result
+}
+
+answer = powerfind(2,4)
+console.log(answer)
+
+
+
+
+
+// function power() {
+//     let 
+//     for (let i = 1; i <= pow; i++ ){
+//         num = num * i
+//     }
+// }
+
+// console.log(power(3,3))
+
+
+//-------------------------------
+
+
+// without arrow funcions
+// Finding the power of a number
+
+// let surya = (a,b) => {
+//     let ans = 1
+//    for (let i = 1; i <= b; i++){
+//           ans = ans * a 
+//    }
+//    return ans
+// }
+
+
+// let answer = surya (3,2)
+//   console.log('The answer is: ' + answer)
+
+
+// //   Ouput:
+// // The answer is: 9
