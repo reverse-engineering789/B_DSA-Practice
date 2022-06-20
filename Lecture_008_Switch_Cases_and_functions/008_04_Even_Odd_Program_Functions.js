dc1e1284fe597c7471790ff8e22a12f82cc73f84
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-20 22:16:31 +0530

    Lecture_008_Switch_Cases_and_functions/008_04_Even_Odd_Program_Functions.js

--- Lecture_008_Switch_Cases_and_functions/008_04_Even_Odd_Program_Functions.js
+++ Lecture_008_Switch_Cases_and_functions/008_04_Even_Odd_Program_Functions.js
@@ -1,14 +1,28 @@
-const EvenOdd = (a) => {
-    if (a % 2 === 0){
-        console.log('The number is even')
-    }else{
-        console.log('The number is odd')
-    }
+// const EvenOdd = (a) => {
+//     if (a % 2 === 0){
+//         console.log('The number is even')
+//     }else{
+//         console.log('The number is odd')
+//     }
+// }
+
+// EvenOdd(4)
+// EvenOdd(1)
+
+// // Output:
+// // The number is even
+// // The number is odd
+
+
+// ------------------------------------
+
+const n = 24
+
+if (n % 2 === 0){
+    console.log('odd')
+}else{
+    console.log('even')
 }
 
-EvenOdd(4)
-EvenOdd(1)
-
-// Output:
-// The number is even
-// The number is odd
+// output:
+// odd
