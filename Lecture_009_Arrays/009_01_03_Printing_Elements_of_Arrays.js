b8fcd3576ee6bfeebede4eb698ea4f301baeb11e
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-21 01:09:18 +0530

    Lecture_009_Arrays/009_01_03_Printing_Elements_of_Arrays.js

--- Lecture_009_Arrays/009_01_03_Printing_Elements_of_Arrays.js
+++ Lecture_009_Arrays/009_01_03_Printing_Elements_of_Arrays.js
@@ -1,12 +1,36 @@
-let list = [2,4,6,8,10]
+// let list = [2,4,6,8,10]
 
-for (let i = 0; i<=list.length-1; i++ ){
-    console.log(list[i])
+// for (let i = 0; i<=list.length-1; i++ ){
+//     console.log(list[i])
+// }
+
+// // Output:
+// // 2
+// // 4
+// // 6
+// // 8
+// // 10
+
+// //----------------------------
+
+const arr = [2,4,56,8,10]
+
+console.log(arr[1], arr[0])
+
+// output:
+// 4 2
+
+
+// accessing the array:
+
+
+const surya = [2,4,6,8,10]
+string = ''
+for (let i = 0; i <= surya.length-1; i++){
+    string = string + surya[i] + ' '
 }
 
+console.log(string)
+
 // Output:
-// 2
-// 4
-// 6
-// 8
-// 10
+// 2 4 6 8 10
