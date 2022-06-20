91b6ec918b155ac03d6fb94d481fe410f4fd1a0c
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-21 01:10:46 +0530

    Lecture_009_Arrays/009_02_Arrays_With_Fuctions.js

--- Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array_Using_Functions.js
+++ Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array_Using_Functions.js
@@ -1,27 +1,65 @@
-// using Functions: 
+// // using Functions: 
 
-let getMin = (arr) => {
+// let getMin = (arr) => {
+//     let min = arr[0]
+//   for(let i =0; i <= arr.length; i++){
+//       if (arr[i] < min){
+//           min = arr[i]
+//       }
+//   }
+//   return min
+// }
+
+// let getMax = (arr) => {
+//     let max = arr[0]
+//     for (let i = 0; i <= arr.length; i++){
+//         if (arr[i] > max){
+//             max = arr[i]
+//         }
+//     }
+//     return max
+// }
+
+
+// let arr1 = [2,4,6,8]
+
+// console.log(getMax(arr1))
+// console.log(getMin(arr1))
+
+
+// // -----------------------------------------
+
+
+function findMin(arr){
     let min = arr[0]
-  for(let i =0; i <= arr.length; i++){
-      if (arr[i] < min){
-          min = arr[i]
-      }
-  }
-  return min
-}
-
-let getMax = (arr) => {
-    let max = arr[0]
-    for (let i = 0; i <= arr.length; i++){
-        if (arr[i] > max){
-            max = arr[i]
-        }
+    for (let i = 0; i <= arr.length-1; i++){
+           if (arr[i] < min){
+               min = arr[i]
+           }
     }
-    return max
+    console.log('min: ' + min)
 }
 
+const findMax = (arr) => {
+    let max = arr[0]
+    for (let i = 0; i <= arr.length-1; i++){
+           if (arr[i] > max){
+               max = arr[i]
+           }   
+    }
+    console.log('max: '+ max)
+}
 
-let arr1 = [2,4,6,8]
+const arrMin = [1,2,3,4,5,6,0]
+findMin(arrMin)
+const arrMax = [1,2,3,4,5,0,6]
+findMax(arrMax)
 
-console.log(getMax(arr1))
-console.log(getMin(arr1))
+// Output:
+// min: 0
+// max: 6
+
+
+//-----------------------
+
+
