bbf2637e4b5cecbe28d3c16a167059613e017f69
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-20 22:11:34 +0530

    Lecture_008_Switch_Cases_and_functions/008_09_Pass_By_Value.js

---
+++ Lecture_008_Switch_Cases_and_functions/008_09_Pass_By_Value.js
@@ -0,0 +1,31 @@
+// let dummy = (n) => {
+//     n++
+//     console.log('n is: ' + n)
+// }
+
+// let n = 4
+
+// dummy(n)
+
+// console.log('n is: ' + n)
+
+// // Output:
+// // n is: 5
+// // n is: 4
+
+
+// // ---------------------------------------------
+
+
+
+let pbv = (n) => {
+     n++
+     console.log(n)
+}
+
+let n = 4 
+
+pbv(n)
+
+console.log('number n is:' + n)
+
