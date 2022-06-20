c9075c303e028bc84230c63951bb54c9e20fc7b1
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-21 01:09:43 +0530

    Lecture_009_Arrays/009_01_Arrays_Implimentation.js

--- Lecture_009_Arrays/009_01_Arrays_Implimentation.js
+++ Lecture_009_Arrays/009_01_Arrays_Implimentation.js
@@ -1,19 +1,30 @@
-const test = [2,4,6,8]
+// const test = [2,4,6,8]
 
-console.log('The value of index 1 is: ' + test[1])
-console.log('The value of index 2 is: ' + test[2])
+// console.log('The value of index 1 is: ' + test[1])
+// console.log('The value of index 2 is: ' + test[2])
 
-// Output:
-// The value of index 1 is: 4
-// The value of index 2 is: 6
+// // Output:
+// // The value of index 1 is: 4
+// // The value of index 2 is: 6
  
 
-for(let i = 0; i <= test.length-1; i++){
-   console.log(test[i])    
+// for(let i = 0; i <= test.length-1; i++){
+//    console.log(test[i])    
+// }
+
+// // Output:
+// // 2
+// // 4
+// // 6
+// // 8
+
+// // ------------------------------
+
+const arr= [4,6,10,16,18]
+str = ''
+
+for (let i = 0; i <arr.length-1; i++){
+   str = str + arr[i] + ' '
 }
 
-// Output:
-// 2
-// 4
-// 6
-// 8
+console.log(str)
