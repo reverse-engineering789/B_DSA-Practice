933883ee819446301e1c1257cef8a67cab6ceeb3
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-25 15:20:57 +0530

    Lecture_004_02_Extra_Patterns_numbers/004_02_05_Extra_Pattern-5.js

--- Lecture_004_02_Extra_Patterns_numbers/004_02_05_Extra_Pattern-5.js
+++ Lecture_004_02_Extra_Patterns_numbers/004_02_05_Extra_Pattern-5.js
@@ -28,3 +28,24 @@
 
 
 
+const n = 5 
+let string = ''
+
+for (let i = 1; i <= n; i++){
+    for (let j = 1; j <= n-i; j++){
+        string += ' '
+    }
+    for (let k = 1; k <= ((2*i)-1); k++){
+        string += + k
+    }
+    string = string + '\n'
+}
+
+console.log(string)
+
+// Output:
+//     1
+//    123
+//   12345
+//  1234567
+// 123456789
