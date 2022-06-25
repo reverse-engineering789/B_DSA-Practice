9dbac931806f27ce9e6c0b55faf40af393023735
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-25 15:21:44 +0530

    Lecture_004_06_Patterns_Practice_Star/004_06_03_Pattern_Star_03.js

--- Lecture_004_06_Patterns_Practice_Star/004_06_03_Pattern_Star_03.js
+++ Lecture_004_06_Patterns_Practice_Star/004_06_03_Pattern_Star_03.js
@@ -1,17 +1,46 @@
+// const n = 5 
+
+// string = ''
+
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j <= n-i; j++){
+//          string = string + ' '
+//     }
+//         for (let k = 1; k <= (i); k++){
+//            string = string + '*'
+//         }
+//         string = string + '\n'
+// }
+
+// console.log(string)
+
+
+// // Output:
+// //     *
+// //    **
+// //   ***
+// //  ****
+// // *****
+
+
+// // ------------------------------------------
+
+
+
 const n = 5 
 
 string = ''
 
-for (let i = 1; i <= n; i++){
-    for (let j = 1; j <= n-i; j++){
-         string = string + ' '
-    }
-        for (let k = 1; k <= (i); k++){
-           string = string + '*'
-        }
-        string = string + '\n'
-}
 
+for (let i = 0; i <= n; i++){
+    for(let j = 1; j <= n-i; j++){
+        string = string + ' '
+    }
+    for (let k = 1; k <= i; k++){
+        string = string + '*'
+    }
+    string = string + '\n'
+}
 console.log(string)
 
 
