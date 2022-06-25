cfee2d62d1556f0cb55862de11719279b29ce0c4
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-25 15:21:19 +0530

    Lecture_004_06_Patterns_Practice_Star/004_06_04_Pattern_Star_04.js

--- Lecture_004_06_Patterns_Practice_Star/004_06_04_Pattern_Star_04.js
+++ Lecture_004_06_Patterns_Practice_Star/004_06_04_Pattern_Star_04.js
@@ -1,30 +1,43 @@
-const n = 5 
+// const n = 5 
 
-for (let i = 1; i <= n; i++){
-    for (let j = 1; j <= i; j++ ){
-        console.log('*')
-    }
-    console.log(' ')
-}
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j <= i; j++ ){
+//         console.log('*')
+//     }
+//     console.log(' ')
+// }
 
 
-// Output:
-// *
+// // Output:
+// // *
  
-// *
-// *
+// // *
+// // *
 
-// *
-// *
-// *
+// // *
+// // *
+// // *
 
-// *
-// *
-// *
-// *
+// // *
+// // *
+// // *
+// // *
 
-// *
-// *
-// *
-// *
-// *
+// // *
+// // *
+// // *
+// // *
+// // *
+
+
+
+const n = 5 
+string = ''
+
+for (let i = 1; i <=n; i++){
+    for (let j = 1; j <= i; j++){
+        string = string + '*'
+    }
+    string = string + '\n'
+}
+console.log(string)
