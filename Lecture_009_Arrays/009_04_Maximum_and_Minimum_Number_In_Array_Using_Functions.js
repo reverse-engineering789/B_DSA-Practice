9e0a7564015e5d6d0fb84053da149d7ff2fa0f86
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-25 15:23:23 +0530

    Lecture_004_02_Extra_Patterns_numbers/004_02_07_Extra_Pattern-7.js

--- Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array_Using_Functions.js
+++ Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array_Using_Functions.js
@@ -27,39 +27,69 @@
 // console.log(getMin(arr1))
 
 
-// // -----------------------------------------
+// // // -----------------------------------------
 
 
-function findMin(arr){
-    let min = arr[0]
-    for (let i = 0; i <= arr.length-1; i++){
-           if (arr[i] < min){
-               min = arr[i]
-           }
-    }
-    console.log('min: ' + min)
-}
+// function findMin(arr){
+//     let min = arr[0]
+//     for (let i = 0; i <= arr.length-1; i++){
+//            if (arr[i] < min){
+//                min = arr[i]
+//            }
+//     }
+//     console.log('min: ' + min)
+// }
+
+// const findMax = (arr) => {
+//     let max = arr[0]
+//     for (let i = 0; i <= arr.length-1; i++){
+//            if (arr[i] > max){
+//                max = arr[i]
+//            }   
+//     }
+//     console.log('max: '+ max)
+// }
+
+// const arrMin = [1,2,3,4,5,6,0]
+// findMin(arrMin)
+// const arrMax = [1,2,3,4,5,0,6]
+// findMax(arrMax)
+
+// // Output:
+// // min: 0
+// // max: 6
+
+
+// //-----------------------
+
+
+
 
 const findMax = (arr) => {
     let max = arr[0]
-    for (let i = 0; i <= arr.length-1; i++){
+    for (let i = 0; i <= arr.length - 1; i++){
            if (arr[i] > max){
                max = arr[i]
-           }   
+           }
     }
-    console.log('max: '+ max)
+    return 'max: ' + max
 }
 
-const arrMin = [1,2,3,4,5,6,0]
-findMin(arrMin)
-const arrMax = [1,2,3,4,5,0,6]
-findMax(arrMax)
+const findMin = (arr) => {
+    let min = arr[0]
+    for (let j = 0; j <= arr.length-1; j++){
+        if (arr[j] < min){
+            min = arr[j]
+        }
+    }
+    return 'min: ' + min
+}
+
+
+arr1 = [1,2,6,0,3,-2,7]
+console.log(findMax(arr1))
+console.log(findMin(arr1))
 
 // Output:
-// min: 0
-// max: 6
-
-
-//-----------------------
-
-
+// max: 7
+// min: -2
