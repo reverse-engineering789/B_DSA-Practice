9a9c0dd368fa8265d8c34c75ba53badc9ea0d09b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-27 18:56:59 +0530

    Lecture_010_Leetcode_CodeStudio_Arrays/010_02_Find_Unique_Element.js

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_02_Find_Unique_Element.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_02_Find_Unique_Element.js
@@ -1,19 +1,55 @@
+// let findUnique = (arr) => {
+//     let ans = 0
+
+//     for (let i = 0; i < arr.length; i++){
+//         ans = ans ^ arr[i]
+//     }
+//     return ans
+// }
+
+
+// let testC1 = [2,4,6,8,2,4,6]
+
+
+// console.log(findUnique(testC1))
+
+
+// // Output:
+// // 8
+// // 5
+
+
+// const findUnique = (arr) => {
+//     let ans = 0
+//   for (let i = 0; i <= arr.length-1; i++){
+//          ans = ans^arr[i]
+//   }
+//   return ans
+// }
+
+// testCase1 = [2,4,3,4,5,6,5,7,6]
+
+// console.log(findUnique(testCase1)) 
+
+
+
+
+
+
 let findUnique = (arr) => {
     let ans = 0
 
-    for (let i = 0; i < arr.length; i++){
-        ans = ans ^ arr[i]
+    for(let i = 0; i < arr.length; i++){
+        ans = ans^arr[i]
     }
     return ans
 }
 
 
-let testC1 = [2,4,6,8,2,4,6]
 
-
-console.log(findUnique(testC1))
-
-
-// Output:
-// 8
-// 5
+let test1 = [2,4,7,2,7]
+let test2 = [1,3,1,3,6,6,7,10,7]
+let test3 = [2,4,3,4,5,6,5,7,6]
+console.log('the unique element in test1 is: ' + findUnique(test1))
+console.log('the unique element in tetestst2 is: ' + findUnique(test2))
+console.log('the unique element in test3 is: ' + findUnique(test3))
