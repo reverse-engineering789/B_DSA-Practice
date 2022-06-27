197c5c85f9f6d5c3be41dfa966a8df0d77e187be
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-27 18:56:44 +0530

    Lecture_010_Leetcode_CodeStudio_Arrays/010_04_02_Array_Intersection.js

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_04_02_Array_Intersection.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_04_02_Array_Intersection.js
@@ -39,31 +39,31 @@ console.log(findArrayIntersection(arr1,arr2))
 
 
 
-let findArrayIntersection = (arr1,arr2) => {
-    let ans = 0
-    // Loop for array1
-    for(let i = 0; i < arr1.length; i++) {
+// let findArrayIntersection = (arr1,arr2) => {
+//     let ans = 0
+//     // Loop for array1
+//     for(let i = 0; i < arr1.length; i++) {
          
-        // Loop for array2
-        for(let j = 0; j < arr2.length; j++) {
+//         // Loop for array2
+//         for(let j = 0; j < arr2.length; j++) {
              
-            // Compare the element of each and
-            // every element from both of the
-            // arrays
-            if(arr1[i] === arr2[j]) {
+//             // Compare the element of each and
+//             // every element from both of the
+//             // arrays
+//             if(arr1[i] === arr2[j]) {
              
-                // Return if common element found
-                return true;
-            }
-        }
+//                 // Return if common element found
+//                 return true;
+//             }
+//         }
 
-    }
-    return false
-}
+//     }
+//     return false
+// }
 
     
  
-let arr1 = [1,2,4,5,6,8,10]
-let arr2 = [2,6,8,14]
+// let arr1 = [1,2,4,5,6,8,10]
+// let arr2 = [2,6,8,14]
 
-console.log(findArrayIntersection(arr1,arr2))
+// console.log(findArrayIntersection(arr1,arr2))
