70d68f30da2b555c79255f5cf117b6874e2f8a79
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-27 18:57:20 +0530

    Lecture_010_Leetcode_CodeStudio_Arrays/010_04_03_Pair_Sum.js

---
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_04_03_Pair_Sum.js
@@ -0,0 +1,60 @@
+
+
+const pairSum = (arr, sum) => {
+    let   count = 0
+    let sumToatal = []
+         for (let i = 0; i <= arr.length-1; i++){
+             for (let j = i+1; j <= arr.length-1; j++){
+                 if (arr[i] + arr[j] === sum){
+                     sumToatal.push(arr[i], arr[j])
+                       count++ 
+                   console.log("(" + arr[i] + ", " + arr[j] + ")" );
+                 }
+             }
+         }
+         return count
+      }
+  
+      
+  
+  const number = 6
+  const array = [1,2,4,5,6,3]
+  console.log(pairSum(array, number))
+  
+  
+// // Output:
+// (1, 5)
+// (2, 4)
+// 2
+
+
+
+// // ----------------------------------------------
+
+
+
+// // gfg -  Print all pairs with given sum
+// // JavaScript implementation of simple method to
+// // find print pairs with given sum.
+
+// // Returns number of pairs in arr[0..n-1]
+// // with sum equal to 'sum'
+// function printPairs(arr, n, sum)
+// {
+// 	let count = 0; // Initialize result
+
+// 	// Consider all possible pairs and check
+// 	// their sums
+// 	for (let i = 0; i < n; i++)
+// 		for (let j = i + 1; j < n; j++)
+// 			if (arr[i] + arr[j] == sum)
+// 				console.log("(" + arr[i] + ", "
+// 					+ arr[j] + ")" );
+// }
+
+// // Driver function to test the above function
+
+// 	let arr = [ 1, 5, 7, -1, 5 ];
+// 	let n = arr.length;
+// 	let sum = 6;
+// 	printPairs(arr, n, sum);
