03bb569a11a7fe8b44b64fd7d0182701bd2d54b7
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-27 18:56:23 +0530

    Lecture_010_Leetcode_CodeStudio_Arrays/010_04_01_01_Array_intersection.js

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_04_01_01_Array_intersection.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_04_01_01_Array_intersection.js
@@ -1,26 +1,54 @@
-let findIntersection = (arr1, arr2) => {
-    let ans = []
-      for (let i = 0; i < arr1.length; i++){
-         let element = arr1[i]
+// let findIntersection = (arr1, arr2) => {
+//     let ans = []
+//       for (let i = 0; i < arr1.length; i++){
+//          let element = arr1[i]
     
-      for (let j = 0; j < arr2.length; j++){
+//       for (let j = 0; j < arr2.length; j++){
 
-        if (element === arr2[j]){
-            ans.push(element)
-            arr2[j] = -132345
-            break
+//         if (element === arr2[j]){
+//             ans.push(element)
+//             arr2[j] = -132345
+//             break
+//         }
+//       }
+//       }
+//       return ans
+// }
+
+// let arr1 = [2,4,6,8,10]
+// let arr2 = [1,4,6,12,14]
+
+
+// let test = findIntersection(arr1, arr2)
+// console.log(test)
+
+// // Output:
+// // [ 4, 6 ]
+
+
+// // ---------------------------------------
+
+
+const findIntersection = (arr1, arr2) => {
+      interSection = []
+      for (let i = 0; i <= arr1.length-1; i++){
+        for (let j = 0; j <= arr2.length-1; j++){
+                if (arr1[i] === arr2[j]){
+                     interSection.push(arr1[i])
+                }
         }
       }
-      }
-      return ans
+      console.log(interSection)
 }
 
-let arr1 = [2,4,6,8,10]
-let arr2 = [1,4,6,12,14]
 
 
-let test = findIntersection(arr1, arr2)
-console.log(test)
+const array1 = [2,4,6,8,10,20
+]
+const array2 = [2,4,5,7,11,20]
+
+findIntersection(array1, array2)
+
 
 // Output:
-// [ 4, 6 ]
+// [ 2, 4, 20 ]
