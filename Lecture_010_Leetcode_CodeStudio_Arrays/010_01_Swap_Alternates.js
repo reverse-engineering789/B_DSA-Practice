6ae73d4250870b8ef036c8c7d8a283a8b6e70ae5
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-27 18:55:59 +0530

    Lecture_010_Leetcode_CodeStudio_Arrays/010_01_Swap_Alternates.js

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_01_Swap_Alternates.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_01_Swap_Alternates.js
@@ -1,30 +1,62 @@
-let swapAlternate = (arr) => {
-    for (let i = 0; i < arr.length; i = i+ 2){
-        if (i+1 < arr.length){
-            temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
+// let swapAlternate = (arr) => {
+//     for (let i = 0; i < arr.length; i = i+ 2){
+//         if (i+1 < arr.length){
+//             temp = arr[i]
+//             arr[i] = arr[i+1]
+//             arr[i+1] = temp
         
+//         }
+//     }
+// }
+
+
+// let printArray = (arr) => {
+//     for (let i = 0; i < arr.length; i++){
+//         console.log(arr[i])
+//     }
+// }
+
+
+// let arr1 = [2,4,6,8,10]
+
+// swapAlternate(arr1)
+// printArray(arr1)
+
+// // Output:
+// // 4
+// // 2
+// // 8
+// // 6
+// // 10
+
+
+let swapAlter = (arr) => {
+    for (let i = 0; i < arr.length; i = i+2){
+        if (i+1 < (arr.length)){
+         temp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = temp
         }
     }
 }
 
-
 let printArray = (arr) => {
-    for (let i = 0; i < arr.length; i++){
-        console.log(arr[i])
-    }
+        for (let i = 0; i < arr.length; i++){
+            console.log(arr[i])
+        }
 }
 
 
-let arr1 = [2,4,6,8,10]
-
-swapAlternate(arr1)
+const arr1 = [2,4,6,8,10,12,14]
+swapAlter(arr1)
 printArray(arr1)
+
 
 // Output:
 // 4
 // 2
 // 8
 // 6
+// 12
 // 10
+// 14
