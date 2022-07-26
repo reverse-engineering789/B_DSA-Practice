a2d2b6cebaab9468032c47b5ff1318a1522e7049
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-07-26 22:45:20 +0530

    Lecture_022_All_About_Char_Arrays_and_Strings/022_02_02_Reverse_a_String.js

---
+++ Lecture_022_All_About_Char_Arrays_and_Strings/022_02_02_Reverse_a_String.js
@@ -0,0 +1,38 @@
+// let  string = ''
+// const reverseString = (str) => {
+//     for (let i = str.length-1; i >= 0; i--){
+//         string = string + str[i]
+//     }
+//     console.log(string)
+// }
+
+
+//  reverseString('Surya')
+
+// //  Output:
+// //  ayruS
+
+
+// // ...............................................
+
+
+// const name = 'Surya'
+// let string = ''
+// for (let i = name.length-1; i >= 0 ; i--){
+//        string = string + name[i]
+// }
+
+// console.log(string)
+
+
+// ................................................
+
+
+const n = 'suyra'
+string = ''
+
+for (let i =  n.length-1; i >= 0; i--){
+    string = string + n[i]
+}
+
+console.log(string)
