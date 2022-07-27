93e8b84780edaf928b46bebfbd60867dd15781ec
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-07-27 11:56:36 +0530

    Lecture_022_All_About_Char_Arrays_and_Strings/022_01_Calculate_the_Length_of_the_String.js

--- Lecture_022_All_About_Char_Arrays_and_Strings/022_01_Calculate_the_Length_of_the_String.js
+++ Lecture_022_All_About_Char_Arrays_and_Strings/022_01_Calculate_the_Length_of_the_String.js
@@ -1,9 +1,28 @@
-let calulateLength = (arr) => {
-    let count = 0  
-    for (let i = 0; i < arr.length; i++){
-         count++
+// let calulateLength = (arr) => {
+//     let count = 0  
+//     for (let i = 0; i < arr.length; i++){
+//          count++
+//     }
+//     return count
+// }
+
+// console.log(calulateLength('Surya') )
+
+
+
+// -------------------------------------------------------------
+
+
+const calculateLength = (str) => {
+    let count = 0
+    for (let i = 0; i <= str.length-1; i++){
+        count++
     }
     return count
 }
 
-console.log(calulateLength('Surya') )
+console.log(calculateLength('Surya'))
+
+
+// Output:
+// 5
