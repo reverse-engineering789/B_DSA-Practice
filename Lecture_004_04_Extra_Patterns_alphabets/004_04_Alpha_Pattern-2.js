1938a452b09c5ed13e2603dd4f0feff25d6d290b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-04 01:40:35 +0530

    Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-2.js

--- Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-2.js
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-2.js
@@ -1,21 +1,21 @@
-let n = 5;
-let string = "";
-// External loop
-for (let i = 1; i <= n; i++) {
-  // printing characters
-  for (let j = 0; j < i; j++) {
-    string += String.fromCharCode((i - 1) + 65);
-  }
-  string += "\n";
-}
-console.log(string);
+// let n = 5;
+// let string = "";
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   // printing characters
+//   for (let j = 0; j < i; j++) {
+//     string += String.fromCharCode((i - 1) + 65);
+//   }
+//   string += "\n";
+// }
+// console.log(string);
 
-// Output:
-// A
-// B B
-// C C C
-// D D D D
-// E E E E E
+// // Output:
+// // A
+// // B B
+// // C C C
+// // D D D D
+// // E E E E E
 
 
 
@@ -45,3 +45,20 @@ console.log(string);
 // // CCC
 // // DDDD
 // // EEEEE
+
+
+
+// ------------------------------------------------------------
+
+
+
+const n = 5
+string = ''
+for (let i = 1; i <= n; i++){
+  for (let j = 1; j <= i; j++){
+    string = string + String.fromCharCode((i)+64)
+  }
+  string = string + '\n'
+}
+
+console.log(string)
