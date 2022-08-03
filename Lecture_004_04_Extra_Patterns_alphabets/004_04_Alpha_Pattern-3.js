0610ccb2a4eec292e7c6c040d7cd2cb2a8899422
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-04 01:40:55 +0530

    Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-3.js

--- Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-3.js
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-3.js
@@ -1,21 +1,21 @@
-let n = 5; // you can take input using prompt or change the value
-let string = "";
-let count = 0;
-// External loop
-for (let i = 1; i <= n; i++) {
-  for (let j = 0; j < i; j++) {
-    string += String.fromCharCode(count + 65);
-    count++; // increment cause next alphabet
-  }
-  string += "\n";
-}
-console.log(string);
-// Output:
-// A
-// B C
-// D E F
-// G H I J
-// K L M N O
+// let n = 5; // you can take input using prompt or change the value
+// let string = "";
+// let count = 0;
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += String.fromCharCode(count + 65);
+//     count++; // increment cause next alphabet
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+// // Output:
+// // A
+// // B C
+// // D E F
+// // G H I J
+// // K L M N O
 
 
 // // My code: \
@@ -40,3 +40,22 @@ console.log(string);
 // // DEF
 // // GHIJ
 // // KLMNO
+
+
+//--------------------------------------------------
+
+
+
+const n  = 5; 
+string = ''
+let count = 1
+
+for (let i = 1; i <= n; i++){
+  for (let j = 1; j <= i; j++){ 
+    string = string + String.fromCharCode(count + 64)
+    count++
+  }
+ string = string + ' '
+}
+
+console.log(string)
