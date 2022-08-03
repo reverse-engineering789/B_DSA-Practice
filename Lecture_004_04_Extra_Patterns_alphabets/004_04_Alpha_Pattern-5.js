8ef73788a4635ce44e4c5e4d23753fc786562126
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-04 01:41:22 +0530

    Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-5.js

--- Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-5.js
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-5.js
@@ -1,13 +1,13 @@
-let n = 5; // you can take input using prompt or change the value
-let string = "";
-// External loop
-for (let i = 1; i <= n; i++) {
-  for (let j = 0; j < n - i + 1; j++) {
-    string += String.fromCharCode((n - 1 - j) + 65);
-  }
-  string += "\n";
-}
-console.log(string);
+// let n = 5; // you can take input using prompt or change the value
+// let string = "";
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < n - i + 1; j++) {
+//     string += String.fromCharCode((n - 1 - j) + 65);
+//   }
+//   string += "\n";
+// }
+// console.log(string);
 // Output:
 // EDCBA
 // EDCB
@@ -20,7 +20,7 @@ console.log(string);
 // string = '';
 // for (let i = 1; i <= n; i++){
 //     for (let j = 1; j <= n-i+1; j++){
-//        string = string + String.fromCharCode((n+1-j)+64)
+//        string = string + String.fromCharCode((n+i-j)+64)
 //     }
 //     string = string + '\n'
 // }
@@ -32,3 +32,23 @@ console.log(string);
 // // EDC
 // // ED
 // // E
+
+
+
+
+let n = 5
+let string = ''
+for(let i = 1; i <= n; i++){
+  for (let j= 1; j <= n-i+1; j++){
+    string = string + String.fromCharCode((n+1-j)+65)
+  }
+  string = string + '\n'
+}
+console.log(string)
+
+// OUTPUT:
+// FEDCB
+// FEDC
+// FED
+// FE
+// F
