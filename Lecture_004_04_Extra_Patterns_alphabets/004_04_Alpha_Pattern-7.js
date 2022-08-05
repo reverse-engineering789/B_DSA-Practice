01da2bc4f66d824c533cb34650eac59bafc2cc28
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-05 11:56:58 +0530

    Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-7.js

--- Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-7.js
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-7.js
@@ -1,18 +1,18 @@
-let n = 5;
-let string = "";
-// External loop
-for (let i = 1; i <= n; i++) {
-  // creating spaces
-  for (let j = 0; j < n - i; j++) {
-    string += " ";
-  }
-  // creating alphabets
-  for (let k = 0; k < 2 * i - 1; k++) {
-    string += String.fromCharCode(k + 65);
-  }
-  string += "\n";
-}
-console.log(string);
+// let n = 5;
+// let string = "";
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   // creating spaces
+//   for (let j = 0; j < n - i; j++) {
+//     string += " ";
+//   }
+//   // creating alphabets
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     string += String.fromCharCode(k + 65);
+//   }
+//   string += "\n";
+// }
+// console.log(string);
 
 
 // Output:
@@ -38,3 +38,33 @@ console.log(string);
 // }
 
 // console.log(string)
+
+// // ---------------------------------------------------------------
+
+
+
+let n = 5
+
+let string = ''
+for (let i = 1; i <= n; i++) {
+    //     string += " ";
+    //   }
+    //   // creating alphabets
+    //   for (let k = 0; k < 2 * i - 1; k++) {
+    //     string += String.fromCharCode(k + 65);
+    //   }
+    //   string += "\n";
+    // }
+    // console.log(string);
+    
+
+  for (let j = 0; j < n - i; j++) {
+      string = string + ' '
+  }
+  for (let k = 0; k < 2 * i - 1; k++) {
+     string = string + String.fromCharCode((k) + 65)
+  }
+  string = string + '\n'
+}
+
+console.log(string)
