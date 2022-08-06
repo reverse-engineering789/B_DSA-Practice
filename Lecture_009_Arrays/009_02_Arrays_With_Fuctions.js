1c317324257b1680c3b6f6a7be8b022b5fe1f923
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-06 16:15:14 +0530

    Lecture_009_Arrays/009_02_Arrays_With_Fuctions.js

--- Lecture_009_Arrays/009_02_Arrays_With_Fuctions.js
+++ Lecture_009_Arrays/009_02_Arrays_With_Fuctions.js
@@ -18,16 +18,38 @@
 // // --------------------------------------------------
 
 
-function printArray(arr){
-    str = ''
-    for (let i = 0; i <= arr; i++){
-        str = str + arr[i] + ' '
+// function printArray(arr){
+//     str = ''
+//     for (let i = 0; i <= arr; i++){
+//         str = str + arr[i] + ' '
+//     }
+//     console.log('Printing is Done - 2nd Code')
+// }
+
+
+// printArray(10)
+
+// // Output:
+// // Printing is Done - 2nd Code
+
+
+
+// // ---------------------------------------------
+
+
+
+function printingArray(hi) {
+    string = ''
+    for (let i = 0; i <= hi.length-1; i++ ){
+       string = string + hi[i] + ' '
     }
-    console.log('Printing is Done - 2nd Code')
+    console.log(string)
 }
 
 
-printArray(10)
+const hi1 = [2,4,6,8]
+printingArray(hi1)
+
 
 // Output:
-// Printing is Done - 2nd Code
+// 2 4 6 8 
