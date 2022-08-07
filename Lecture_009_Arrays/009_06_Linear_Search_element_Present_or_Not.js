4d07393b2546afde0426681e080b50f7b7f6c46f
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-08 01:55:01 +0530

    Lecture_009_Arrays/009_06_Linear_Search_element_Present_or_Not.js

--- Lecture_009_Arrays/009_06_Linear_Search_element_Present_or_Not.js
+++ Lecture_009_Arrays/009_06_Linear_Search_element_Present_or_Not.js
@@ -1,20 +1,48 @@
-const search = (arr, key) => {
-     for (let i = 0; i < arr.length; i++)   {
-         if (arr[i] === key){
-             return 1
-         }
-     }
-     return 0
+// const search = (arr, key) => {
+//      for (let i = 0; i < arr.length; i++)   {
+//          if (arr[i] === key){
+//              return 1
+//          }
+//      }
+//      return 0
+// }
+
+// const Array = [2,4,6,8,10]
+
+// const number = 4
+
+// const test = search(Array, number)
+
+// if (test) {
+//   console.log(number + ' is present')
+// }else{
+//   console.log(number + ' is absent')
+// }
+
+// // Output:
+// // 4 is present
+
+
+// //--------------------------------------------------------------------
+
+
+function test (n, item)  {
+  for(let i = 0; i <= n.length-1; i++){
+    if (n[i] === item){
+      return `${item} is present`
+    }
+  }
+return `${item} is not present`
 }
+Array = [2,4,6,8,10]
+console.log(test(Array, 4))
+console.log(test(Array, 5))
 
-const Array = [2,4,6,8,10]
+// Output:
+// 4 is present
+// 5 is not present
 
-const number = 4
+// //--------------------------------------------------------------------
 
-const test = search(Array, number)
 
-if (test) {
-  console.log(number + ' is present')
-}else{
-  console.log(number + ' is absent')
-}
+
