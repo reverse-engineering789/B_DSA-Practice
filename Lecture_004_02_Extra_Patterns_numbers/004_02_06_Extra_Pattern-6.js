fc8c8fda04a34c051d96dbaa2a8b9b0c6cc7fdf2
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-09 08:57:01 +0530

    Lecture_004_02_Extra_Patterns_numbers/004_02_06_Extra_Pattern-6.js

--- Lecture_004_02_Extra_Patterns_numbers/004_02_06_Extra_Pattern-6.js
+++ Lecture_004_02_Extra_Patterns_numbers/004_02_06_Extra_Pattern-6.js
@@ -1,23 +1,53 @@
-let n = 3;
-let count = 1;
-let string = "";
-// External loop
-for (let i = 1; i <= n; i++) {
-  // creating spaces
-  for (let j = 1; j <= n - i; j++) {
-    string += " ";
+// let n = 3;
+// let count = 1;
+// let string = "";
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   // creating spaces
+//   for (let j = 1; j <= n - i; j++) {
+//     string += " ";
+//   }
+//   // creating numbers
+//   for (let k = 1; k <= 2 * i - 1; k++) {
+//     string += count;
+//     count++;
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+// // Output:
+// //    1
+// //   234
+// //  56789
+
+
+
+
+// // ---------------------------------------------------
+
+
+
+const n = 4
+string = ''
+for (let i = 1; i <= n; i++){
+  count = 1
+  for (let j = 1; j <= n-i+1; j++){
+    string = string + ' '
   }
-  // creating numbers
-  for (let k = 1; k <= 2 * i - 1; k++) {
-    string += count;
-    count++;
-  }
-  string += "\n";
+  for (let k = 1; k <= (2*i)-1; k++){
+    string = string + count 
+    count++
+  } 
+  string = string + '\n'
 }
-console.log(string);
+
+console.log(string)
 
 
 // Output:
 //    1
-//   234
-//  56789
+//   123
+//  12345
+// 1234567
