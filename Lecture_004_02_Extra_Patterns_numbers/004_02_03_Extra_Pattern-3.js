3ff566142890e864a8d8fdafe181e6e4b5de3b2f
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-09 08:56:18 +0530

    Lecture_004_02_Extra_Patterns_numbers/004_02_03_Extra_Pattern-3.js

--- Lecture_004_02_Extra_Patterns_numbers/004_02_03_Extra_Pattern-3.js
+++ Lecture_004_02_Extra_Patterns_numbers/004_02_03_Extra_Pattern-3.js
@@ -5,13 +5,39 @@
 // // 1
 
 
-let n = 5; // height of pattern
-let string = "";
-// External loop
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n - i + 1; j++) {
-    string += n - j + 1;
+// let n = 5; // height of pattern
+// let string = "";
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     string += n - j + 1;
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+
+// // ----------------------------------------------------------
+
+
+const n = 5
+string = ''
+for (let i = 1; i <= n; i++){
+  for (let j = 1; j <= n-i+1; j++){
+    string = string + (n-i-j+2) + ' '
   }
-  string += "\n";
+  string = string + '\n'
 }
-console.log(string);
+
+console.log(string)
+
+
+// Output:
+// 5 4 3 2 1 
+// 4 3 2 1
+// 3 2 1
+// 2 1
+// 1
