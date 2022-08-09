705cae551082c9c0f0ed029185ff61871344b342
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-09 08:55:59 +0530

    Lecture_004_02_Extra_Patterns_numbers/004_02_02_Extra_Pattern-2.js

--- Lecture_004_02_Extra_Patterns_numbers/004_02_02_Extra_Pattern-2.js
+++ Lecture_004_02_Extra_Patterns_numbers/004_02_02_Extra_Pattern-2.js
@@ -1,18 +1,35 @@
 
 
-// 4321
-// 321
-// 21
-// 1
+// // 4321
+// // 321
+// // 21
+// // 1
 
 
-const n = 4
+// const n = 4
 
-let string = ''
+// let string = ''
 
-for (let i = 1; i <= n; i++){
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j <= n-i+1; j++){
+//         string += n - j - i + 2
+//     }
+//     string = string + '\n'
+// }
+
+// console.log(string)
+
+
+// // -------------------------------------------
+
+
+
+const n = 5
+string = ''
+
+for(let i = 1; i <= n; i++){
     for (let j = 1; j <= n-i+1; j++){
-        string += n - j - i + 2
+        string = string + (n-i-j+2) + ' '
     }
     string = string + '\n'
 }
@@ -20,4 +37,9 @@ for (let i = 1; i <= n; i++){
 console.log(string)
 
 
-
+// Output:
+// 5 4 3 2 1 
+// 4 3 2 1
+// 3 2 1
+// 2 1
+// 1
