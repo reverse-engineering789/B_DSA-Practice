eaa6bbbc951e6c3914d86a3f1f73f5635296a247
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-10 12:14:26 +0530

    Lecture_004_02_Extra_Patterns_numbers/004_02_09_Extra_Pattern-9.js

--- Lecture_004_02_Extra_Patterns_numbers/004_02_09_Extra_Pattern-9.js
+++ Lecture_004_02_Extra_Patterns_numbers/004_02_09_Extra_Pattern-9.js
@@ -1,35 +1,55 @@
-let n = 5;
-let string = "";
-// Reverse Pyramid
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j < i; j++) {
-    string += " ";
-  }
-  for (let k = 1; k <= 2 * (n - i + 1) - 1; k++) {
-    string += k;
-  }
-  string += "\n";
-}
-// Pyramid
-for (let i = 1; i <= n - 1; i++) {
-  for (let j = 1; j < n - i; j++) {
-    string += " ";
-  }
-  for (let k = 1; k <= 2 * (i + 1) - 1; k++) {
-    string += k;
-  }
-  string += "\n";
-}
-console.log(string);
+// let n = 5;
+// let string = "";
+// // Reverse Pyramid
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j < i; j++) {
+//     string += " ";
+//   }
+//   for (let k = 1; k <= 2 * (n - i + 1) - 1; k++) {
+//     string += k;
+//   }
+//   string += "\n";
+// }
+// // Pyramid
+// for (let i = 1; i <= n - 1; i++) {
+//   for (let j = 1; j < n - i; j++) {
+//     string += " ";
+//   }
+//   for (let k = 1; k <= 2 * (i + 1) - 1; k++) {
+//     string += k;
+//   }
+//   string += "\n";
+// }
+// console.log(string);
 
 
-// Output:
-// 123456789
-//  1234567
-//   12345
-//    123
-//     1
-//    123
-//   12345
-//  1234567
-// 123456789
+// // Output:
+// // 123456789
+// //  1234567
+// //   12345
+// //    123
+// //     1
+// //    123
+// //   12345
+// //  1234567
+// // 123456789
+
+
+
+// // // =============================================
+
+
+
+
+const n = 5 
+string = ''
+for (let i = 1; i <= n; i++){
+  for (let j = 1; j < i; j++){
+       string = string + ' '
+  }
+  for (let k =1; k <= 2 * (n - i + 1) - 1; k++){
+       string = string + k
+  }
+  string = string + '\n'
+}
+console.log(string)
