9a7a5915980286758be87119db17cb28e21f6d4e
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-10 12:14:55 +0530

    Lecture_004_02_Extra_Patterns_numbers/004_02_10_Extra_Pattern-10.js

--- Lecture_004_02_Extra_Patterns_numbers/004_02_10_Extra_Pattern-10.js
+++ Lecture_004_02_Extra_Patterns_numbers/004_02_10_Extra_Pattern-10.js
@@ -1,20 +1,57 @@
-let n = 5;
-let string = "";
-// Pyramid
-for (let i = 1; i <= n; i++) {
-  for (let k = 1; k <= i; k++) {
-    string += k;
+// let n = 5;
+// let string = "";
+// // Pyramid
+// for (let i = 1; i <= n; i++) {
+//   for (let k = 1; k <= i; k++) {
+//     string += k;
+//   }
+//   string += "\n";
+// }
+// // Reverse Pyramid
+// for (let i = 1; i <= n - 1; i++) {
+//   for (let k = 1; k <= n - i; k++) {
+//     string += k;
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+// // Output:
+// // 1
+// // 12
+// // 123
+// // 1234
+// // 12345
+// // 1234
+// // 123
+// // 12
+// // 1
+
+
+
+//============================================================
+
+
+
+
+const n = 5
+string = ''
+for (let i = 1; i <= n; i++){
+  for (let j = 1; j <= i; j++){
+      string = string + j
   }
-  string += "\n";
+  string = string + '\n'
 }
-// Reverse Pyramid
-for (let i = 1; i <= n - 1; i++) {
-  for (let k = 1; k <= n - i; k++) {
-    string += k;
+
+for (let i = 1; i <= n; i++){
+  for (let j = 1; j < n-i+1; j++){
+    string = string + j
   }
-  string += "\n";
+  string = string + '\n'
 }
-console.log(string);
+
+console.log(string)
 
 
 // Output:
