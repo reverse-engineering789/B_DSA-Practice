d13e26e9bd76e784634a6209f8835915f1439ec6
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-12 12:12:18 +0530

    Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_02_02_Find_total_number_of_Accurence.js

--- Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_02_02_Find_total_number_of_Accurence.js
+++ Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_02_02_Find_total_number_of_Accurence.js
@@ -1,66 +1,64 @@
-// let firstOcc = (arr, key) => {
-//     let s = 0;
-//     let e = arr.length-1
-//     let ans = -1
-//     let mid = s + Math.floor(s + (e-s)/ 2)
+let firstOcc = (arr, key) => {
+    let s = 0;
+    let e = arr.length-1
+    let ans = -1
+    let mid = s + Math.floor(s + (e-s)/ 2)
 
 
-//     while (s <= e){
-//         if (arr[mid] === key){
-//         //    console.log(arr[mid])
-//             ans = mid
-//             e = mid - 1
+    while (s <= e){
+        if (arr[mid] === key){
+        //    console.log(arr[mid])
+            ans = mid
+            e = mid - 1
 
-//         }else if (key > arr[mid]){
-//             s = mid + 1
+        }else if (key > arr[mid]){
+            s = mid + 1
             
-//         }else if (key < arr[mid]){
-//             e = mid - 1
-//         }
+        }else if (key < arr[mid]){
+            e = mid - 1
+        }
 
-//         mid = s + Math.floor((e-s)/ 2)
-//        // console.log(mid)
-//     }
-//     return ans
-// }
-
-
-//---------------------------------------------
-
-// let LastOcc = (arr, key) => {
-//    let s = 0;
-//    let e = arr.length-1
-//    let ans = -1
-//    let mid = s + Math.floor(s + (e-s)/ 2)
+        mid = s + Math.floor((e-s)/ 2)
+       // console.log(mid)
+    }
+    return ans
+}
 
 
-//    while (s <= e){
-//        if (arr[mid] === key){
-//          // console.log('arr[mid] '+arr[mid])
-//            ans = mid
-//            s = mid + 1
-//            //console.log('s' + s)
+let LastOcc = (arr, key) => {
+   let s = 0;
+   let e = arr.length-1
+   let ans = -1
+   let mid = s + Math.floor(s + (e-s)/ 2)
 
-//        }else if (key > arr[mid]){
-//            s = mid + 1
+
+   while (s <= e){
+       if (arr[mid] === key){
+         // console.log('arr[mid] '+arr[mid])
+           ans = mid
+           s = mid + 1
+           //console.log('s' + s)
+
+       }else if (key > arr[mid]){
+           s = mid + 1
            
-//        }else if (key < arr[mid]){
-//            e = mid - 1
-//        }
+       }else if (key < arr[mid]){
+           e = mid - 1
+       }
        
-//        mid = s + Math.floor((e-s)/ 2)
-//    //   console.log(mid)
-//    }
-//    return ans
-// }
+       mid = s + Math.floor((e-s)/ 2)
+   //   console.log(mid)
+   }
+   return ans
+}
 
 
-// let even = [1,2,3,3,3,3,3,3,3,3,3,3,5]
-// const firstOccurece =  firstOcc(even, 3)
-// const LastOccurece =  LastOcc(even, 3)
+let even = [1,2,3,3,3,3,3,3,3,3,3,3,5]
+const firstOccurece =  firstOcc(even, 3)
+const LastOccurece =  LastOcc(even, 3)
 
-// const totalOccurece = (LastOccurece -firstOccurece) + 1
-// console.log(totalOccurece)
+const totalOccurece = (LastOccurece -firstOccurece) + 1
+console.log(totalOccurece)
 
 
 // // Output:
@@ -70,21 +68,6 @@
 
 // // ---------------------------------------------
 
-
-const totalOcc = (arr, key) => {
-    let count = 0
-       for (let i = 1; i <= arr.length-1; i++){
-           if (arr[i] === key){
-               count++
-           }
-       } 
-       return count
-}
-
-
-let array = [1,2,3,3,3,3,3,3,3,3,3,3,5]
-const totalOccurece =  totalOcc(array, 5)
-console.log(totalOccurece)
 
 
 
