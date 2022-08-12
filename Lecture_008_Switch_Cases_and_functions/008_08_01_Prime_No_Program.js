c88ebaf83825ac0d2f39e4e30cb8073676609bdb
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-12 12:11:32 +0530

    Lecture_008_Switch_Cases_and_functions/008_08_01_Prime_No_Program.js

--- Lecture_008_Switch_Cases_and_functions/008_08_01_Prime_No_Program.js
+++ Lecture_008_Switch_Cases_and_functions/008_08_01_Prime_No_Program.js
@@ -19,6 +19,18 @@
 //------------------------------------
 
 
+const PrimeOrNot = (number) => {
+    for (let i = 0; i < number; i++) {
+        if (n % i === 0) {
+           return `is a prime number`
+        }
+        }
+        return `is a prime number`
+}
+
+const n = 12
+console.log(PrimeOrNot(n))
+
 
 
 // let isPrime = (number) => {
@@ -49,20 +61,41 @@
 
 
 
-const PrimeOrNot = (n) => {
-    for (let i = 2; i < n; i++){
-        if (n % i === 0){
-            return 0
-        }
-    }
-    return 1
-}
+// const PrimeOrNot = (n) => {
+//     for (let i = 2; i < n; i++){
+//         if (n % i === 0){
+//             return 0
+//         }
+//     }
+//     return 1
+// }
 
 
-let n = 12
+// let n = 12
 
-if (PrimeOrNot(n) === 0){
-    console.log(`not a prime`)
-}else{
-    console.log(`is Prime`)
-}  
+// if (PrimeOrNot(n) === 0){
+//     console.log(`not a prime`)
+// }else{
+//     console.log(`is Prime`)
+// }  
+
+// // not a prime
+// // ----------------------------------------------------
+
+
+
+// 12/8
+
+// const PrimeOrNot = (number) => {
+//     for (let i = 0; i < number; i++) {
+//         if (n % i === 0) {
+//            return `is a prime number`
+//         }
+//         }
+//         return `is a prime number`
+// }
+
+// const n = 12
+// console.log(PrimeOrNot(n))
+
+
