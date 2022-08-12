ba02dd0b063a82047970d7ef016f730a8e548b41
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-12 12:10:20 +0530

    Lecture_004_03_Extra_Patterns_stars/004_03_01_star_Patterns.js

--- Lecture_004_03_Extra_Patterns_stars/004_03_01_star_Patterns.js
+++ Lecture_004_03_Extra_Patterns_stars/004_03_01_star_Patterns.js
@@ -1,17 +1,39 @@
-let n = 5; // row or column count
-// defining an empty string
-let string = "";
+// let n = 5; // row or column count
+// // defining an empty string
+// let string = "";
 
-for(let i = 0; i < n; i++) { // external loop
-  for(let j = 0; j < n; j++) { // internal loop
-    string += "*";
-  }
-  // newline after each row
-  string += "\n";
+// for(let i = 0; i < n; i++) { // external loop
+//   for(let j = 0; j < n; j++) { // internal loop
+//     string += "*";
+//   }
+//   // newline after each row
+//   string += "\n";
+// }
+// // printing the string
+// console.log(string);
+
+
+// // Output:
+// // *****
+// // *****
+// // *****
+// // *****
+// // *****
+
+
+// // ==========================================================
+
+
+const n = 5
+string = ''
+for (let i = 1; i <= n; i++){
+   for(let j = 1; j <= n; j++){
+       string = string + '*'
+   }
+   string = string + '\n'
 }
-// printing the string
-console.log(string);
 
+console.log(string)
 
 // Output:
 // *****
@@ -19,5 +41,3 @@ console.log(string);
 // *****
 // *****
 // *****
-
-
