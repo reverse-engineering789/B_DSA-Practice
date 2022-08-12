6757a1d53c2eae7ca6de60a6f04ffd6b3c082bbf
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-12 12:11:16 +0530

    Lecture_008_Switch_Cases_and_functions/008_07_01_ounting_Program.js

--- Lecture_008_Switch_Cases_and_functions/008_07_01_ounting_Program.js
+++ Lecture_008_Switch_Cases_and_functions/008_07_01_ounting_Program.js
@@ -1,19 +1,32 @@
-const countNum = (number) => {
-for (let i = 1; i <= number; i++){
-    console.log(i)
-}
-}
+// const countNum = (number) => {
+// for (let i = 1; i <= number; i++){
+//     console.log(i)
+// }
+// }
 
-const test = countNum(10)
+// const test = countNum(10)
+
+// // Output:
+// // 1
+// // 2
+// // 3
+// // 4
+// // 5
+// // 6
+// // 7
+// // 8
+// // 9
+// // 10
+
+
+// 12/8
+const n = 5
+
+const string = []
+for (let i = 0; i <= n; i++){
+     string.push(i)
+}
+console.log(string)
 
 // Output:
-// 1
-// 2
-// 3
-// 4
-// 5
-// 6
-// 7
-// 8
-// 9
-// 10
+// [ 0, 1, 2, 3, 4, 5 ]
