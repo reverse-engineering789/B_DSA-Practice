cc5a06eade5ca915d399f1ba6d7b1ebfa47cf612
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-18 07:32:31 +0530

    Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array.js

--- Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array.js
+++ Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array.js
@@ -1,63 +1,63 @@
-// You tube COding Puzzle #1 - Find Max & Min Numbers in Array
-// 009_04_Maximum_and_Minimum_Number_In_Array
-let arr = [1, 100, 10]
+// // You tube COding Puzzle #1 - Find Max & Min Numbers in Array
+// // 009_04_Maximum_and_Minimum_Number_In_Array
+// let arr = [1, 100, 10]
 
-let max = arr[0]
-let min = arr[0]
-
-
-for (let i = 0; i < arr.length; i++ ){
-    if (arr[i] >= max){
-        max = arr[i]
-    }
-}
-console.log(max)
+// let max = arr[0]
+// let min = arr[0]
 
 
-for (let j = 0; j < arr.length; j++ ){
-    if (arr[j] < min){
-        min = arr[j]
-    }
-}
-
-console.log(min)
-
-// Output:
-// 100
-// 1
-
-// //-------------------
+// for (let i = 0; i < arr.length; i++ ){
+//     if (arr[i] >= max){
+//         max = arr[i]
+//     }
+// }
+// console.log(max)
 
 
+// for (let j = 0; j < arr.length; j++ ){
+//     if (arr[j] < min){
+//         min = arr[j]
+//     }
+// }
 
-let numbers = [9,5,78,5,4,3343,6,8,7,56,1,5,6,86,6,]
+// console.log(min)
 
-console.log(Math.max(...numbers))
+// // Output:
+// // 100
+// // 1
 
- // Output:
-// 3343
+// // //-------------------
 
 
 
+// let numbers = [9,5,78,5,4,3343,6,8,7,56,1,5,6,86,6,]
 
+// console.log(Math.max(...numbers))
 
-console.log(Math.min(...numbers))
-
- // Output:
- // 1
+//  // Output:
+// // 3343
 
 
 
 
-// //-------------------------------------------------------------------
+
+// console.log(Math.min(...numbers))
+
+//  // Output:
+//  // 1
 
 
-console.log(Math.max.apply(Math, numbers))
- // Output:
- // 3343
 
 
-// //-------------------------------------------------------------------
+// // //-------------------------------------------------------------------
+
+
+// console.log(Math.max.apply(Math, numbers))
+//  // Output:
+//  // 3343
+
+
+// // //-------------------------------------------------------------------
 
 
 
