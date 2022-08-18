60803e3c5a6e02aa88acc588b39d793f99889b7f
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-18 07:32:49 +0530

    Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array_Using_Functions.js

--- Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array_Using_Functions.js
+++ Lecture_009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array_Using_Functions.js
@@ -97,28 +97,35 @@
 
 // --------------------------------------------------
 
-const findMaxi = (arr) => {
-    let max = arr[0]
-    for (let i = 0; i <= arr.length-1; i++){
-      if (arr[i] > max){
-        max = arr[i+1]
-      }
-    }
-    return max
-}
+// const findMaxi = (arr) => {
+//     let max = arr[0]
+//     for (let i = 0; i <= arr.length-1; i++){
+//       if (arr[i] > max){
+//         max = arr[i+1]
+//       }
+//     }
+//     return max
+// }
 
-const findMini = (arr) => {
-    for(let j = 0; j <= arr.length-1; j++){
-        let min = arr[0]
-        if(arr[i+1] > arr[i]) {
-            min = arr[i+1]
-        }
-    }
-    return min
-}
+// const findMini = (arr) => {
+//     for(let j = 0; j <= arr.length-1; j++){
+//         let min = arr[0]
+//         if(arr[i+1] > arr[i]) {
+//             min = arr[i+1]
+//         }
+//     }
+//     return min
+// }
 
 
-const array1 = [2,4,6,8,10]
+// const array1 = [2,4,6,8,10]
 
-console.log(findMaxi(array1))
-console.log(findMini(array1))
+// console.log(findMaxi(array1))
+// console.log(findMini(array1))
+
+
+
+// //--------------------------------------------------------
+
+
+
