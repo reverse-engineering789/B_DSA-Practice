6754ca80eb45edbe12c2f4f40c61115c74f759fd
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-19 11:11:38 +0530

    Lecture_009_Arrays/sorting_the_array_NEW1.js

--- Lecture_009_Arrays/sorting_the_array_NEW1.js
+++ Lecture_009_Arrays/sorting_the_array_NEW1.js
@@ -1,25 +1,25 @@
 
-const sortArray = (arr) => {
-    for (let i = 0; i <= array.length - 1; i++) {
-        for (let j = i + 1; j <= array.length - 1; j++) {
+// const sortArray = (arr) => {
+//     for (let i = 0; i <= array.length - 1; i++) {
+//         for (let j = i + 1; j <= array.length - 1; j++) {
 
-            if (arr[i] > arr[j]) {
-                let temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
-            }
-        }
-    }
-  return arr
-}
+//             if (arr[i] > arr[j]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[j]
+//                 arr[j] = temp
+//             }
+//         }
+//     }
+//   return arr
+// }
 
-const array = [2, 8, 4, 6, 10]
-const array2 = [10,6,4,8,2]
-console.log(sortArray(array))
-console.log(sortArray(array2))
-// Output:
-// [ 2, 4, 6, 8, 10 ]
-// [ 2, 4, 6, 8, 10 ]
+// const array = [2, 8, 4, 6, 10]
+// const array2 = [10,6,4,8,2]
+// console.log(sortArray(array))
+// console.log(sortArray(array2))
+// // Output:
+// // [ 2, 4, 6, 8, 10 ]
+// // [ 2, 4, 6, 8, 10 ]
 
 
 
@@ -98,6 +98,28 @@ console.log(sortArray(array2))
 // undefined
 
 
+// // -----------------------------------------------------
 
 
 
+
+let sorting = (array) => {
+    for (let i = 0; i <= array.length-1; i++){
+       for (let j = i+1; j <= array.length-1; j++){
+        if (array[i] > array[j]){
+            let temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+        }
+       }
+    }
+    return array
+}
+
+const array1 = [2, 8, 4, 6, 10]
+const array2 = [10,6,4,8,2]
+console.log(sorting(array1))
+console.log(sorting(array2))
+// Output:
+// [ 2, 4, 6, 8, 10 ]
+// [ 2, 4, 6, 8, 10 ]
