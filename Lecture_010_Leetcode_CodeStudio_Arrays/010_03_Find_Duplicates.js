bd28a672fc4384ad470bd69d14aca32dd2678c9a
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-21 08:12:26 +0530

    010_03_Find_Duplicates.js

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_03_Find_Duplicates.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_03_Find_Duplicates.js
@@ -1,7 +1,31 @@
+// My code:
+const findDup = (arr) => {
+    let array = []
+    for (let i = 0; i <= arr.length-1; i++){
+        for (let j = i+1; j <= arr.length-1; j++){
+            if (arr[i] === arr[j]){
+                array.push(arr[i])
+            }
+        }
+    }
+    console.log(array)
+}
 
+let arr1 = [2,5,4,3,2] 
+let arr2 = [6,3,1,5,4,3,2]
+let arr3 = [2,4,3,6,6,3,4,2]
+let arr4 = [2,4,3,6,6,3,4,2,1]
+findDup(arr1)
+findDup(arr2)
+findDup(arr3)
+findDup(arr4)
 
-// 010_03_Find_Duplicates
-// not giving proper output for test case: [2,4,3,6,6,3,4,2]
+// // --------------------------------------------------
+
+//L.b: 
+
+// // 010_03_Find_Duplicates
+// // not giving proper output for test case: [2,4,3,6,6,3,4,2]
 
 
 
@@ -18,8 +42,37 @@ let findDuplicate = (arr) => {
     return ans
 }
 
-let arr1 = [2,5,4,3,2] 
-let arr2 = [6,3,1,5,4,3,2]
+let array1 = [2,5,4,3,2] 
+let array2 = [6,3,1,5,4,3,2]
 
-console.log('The duplicate element in arr1: ' + findDuplicate(arr1))
-console.log('The duplicate element in arr2: ' + findDuplicate(arr2))
+console.log('The duplicate element in arr1: ' + findDuplicate(array1))
+console.log('The duplicate element in arr2: ' + findDuplicate(array2))
+
+
+
+// // --------------------------------------------------
+
+
+// const findDup = (arr) => {
+//     let array = []
+//     for (let i = 0; i <= arr.length-1; i++){
+//         for (let j = i+1; j <= arr.length-1; j++){
+//             if (arr[i] === arr[j]){
+//                 array.push(arr[i])
+//             }
+//         }
+//     }
+//     console.log(array)
+// }
+
+// let arr1 = [2,5,4,3,2] 
+// let arr2 = [6,3,1,5,4,3,2]
+// let arr3 = [2,4,3,6,6,3,4,2]
+// let arr4 = [2,4,3,6,6,3,4,2,1]
+// findDup(arr1)
+// findDup(arr2)
+// findDup(arr3)
+// findDup(arr4)
+
+
+///--------------------------------------------------------------
