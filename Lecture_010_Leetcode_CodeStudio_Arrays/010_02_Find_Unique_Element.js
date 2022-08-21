a712c237fad3d612ee16e04855b9679d18aea4fc
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-21 08:12:08 +0530

    010_02_Find_Unique_Element.js

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_02_Find_Unique_Element.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_02_Find_Unique_Element.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 // let findUnique = (arr) => {
 //     let ans = 0
 
@@ -36,20 +41,20 @@
 
 
 
-let findUnique = (arr) => {
-    let ans = 0
+// let findUnique = (arr) => {
+//     let ans = 0
 
-    for(let i = 0; i < arr.length; i++){
-        ans = ans^arr[i]
-    }
-    return ans
-}
+//     for(let i = 0; i < arr.length; i++){
+//         ans = ans^arr[i]
+//     }
+//     return ans
+// }
 
 
 
-let test1 = [2,4,7,2,7]
-let test2 = [1,3,1,3,6,6,7,10,7]
-let test3 = [2,4,3,4,5,6,5,7,6]
-console.log('the unique element in test1 is: ' + findUnique(test1))
-console.log('the unique element in tetestst2 is: ' + findUnique(test2))
-console.log('the unique element in test3 is: ' + findUnique(test3))
+// let test1 = [2,4,7,2,7]
+// let test2 = [1,3,1,3,6,6,7,10,7]
+// let test3 = [2,4,3,4,5,6,5,7,6]
+// console.log('the unique element in test1 is: ' + findUnique(test1))
+// console.log('the unique element in tetestst2 is: ' + findUnique(test2))
+// console.log('the unique element in test3 is: ' + findUnique(test3))
