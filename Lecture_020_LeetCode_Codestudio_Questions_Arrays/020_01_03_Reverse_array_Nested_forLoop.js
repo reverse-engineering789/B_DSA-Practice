bf36fa79193915494118e10f13a57c11872874b6
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-23 14:11:25 +0530

    020_01_03_Reverse_array_Nested_forLoop.js

--- Lecture_020_LeetCode_Codestudio_Questions_Arrays/020_01_03_Reverse_array_Nested_forLoop.js
+++ Lecture_020_LeetCode_Codestudio_Questions_Arrays/020_01_03_Reverse_array_Nested_forLoop.js
@@ -1,10 +1,32 @@
-let arr = [1, 12, 15, 16, 78, 89, 53];
-for (let i = 0; i < arr.length; i++) {
-  for (let j = i + 1; j < arr.length; j++) {
-    let  b;
-    b = arr[i];
-    arr[i] = arr[j];
-    arr[j] = b;
+// let arr = [1, 12, 15, 16, 78, 89, 53];
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i + 1; j < arr.length; j++) {
+//     let  b;
+//     b = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = b;
+//   }
+// }
+// console.log(arr);
+
+
+
+// // ----------------------------------------------------
+
+
+
+
+const reverseNested = (arr) => {
+  for(let i = 0; i <= arr.length-1; i++){
+    for (let j = i+1; j <= arr.length-1; j++){
+       let temp = arr[i]
+       arr[i] = arr[j]
+       arr[j] = temp
+    }
   }
+  console.log(arr)
 }
-console.log(arr);
+
+const array = [2,4,6,8,10]
+
+reverseNested(array)
