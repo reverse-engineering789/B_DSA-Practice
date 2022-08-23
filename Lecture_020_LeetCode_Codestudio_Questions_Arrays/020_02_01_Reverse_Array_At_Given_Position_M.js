d79fc66429fb9f7735f7d67c3520e1bdc2ab32f4
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-23 14:11:43 +0530

    020_02_01_Reverse_Array_At_Given_Position_M.js

--- Lecture_020_LeetCode_Codestudio_Questions_Arrays/020_02_01_Reverse_Array_At_Given_Position_M.js
+++ Lecture_020_LeetCode_Codestudio_Questions_Arrays/020_02_01_Reverse_Array_At_Given_Position_M.js
@@ -35,27 +35,62 @@
 
 
 // //-----------------------------------------
-let reversedArray = []
-const reversed = (arr, index) => {
-    for (let i = arr.length; i >= index; i--){
-        reversedArray.push(arr[i])   
-    }
-    return reversedArray
+// let reversedArray = []
+// const reversed = (arr, index) => {
+//     for (let i = arr.length; i >= index; i--){
+//         reversedArray.push(arr[i])   
+//     }
+//     return reversedArray
+// }
+
+// let nonReversedArray = []
+// const nonReversed = (arr, index) => {
+//     for (let j = 0; j < index; j++){
+//         nonReversedArray.push(arr[j])
+//     }
+//     return nonReversedArray
+// }
+
+// const completeArray = nonReversedArray.concat(reversedArray)
+
+
+// const testCase1 = [2,4,6,8,10,12,16]
+// const index = 3
+// reversed(testCase1, index)
+// nonReversed(testCase1, index)
+// console.log(completeArray)
+
+
+// // -------------------------------------------------
+
+
+
+const reverseArray = (arr, n) => {
+    let empty1 = []
+    let i = arr.length-1
+     while (i >= n){
+          empty1.push(arr[i])
+        i--
+     }
+    return empty1
 }
 
-let nonReversedArray = []
-const nonReversed = (arr, index) => {
-    for (let j = 0; j < index; j++){
-        nonReversedArray.push(arr[j])
-    }
-    return nonReversedArray
+
+const NONreverseArray = (arr, n) => {
+    empty2 = []
+    let i = 0
+  
+     while (i < n){
+          empty2.push(arr[i])
+          i++
+     }
+     return empty2
 }
 
-const completeArray = nonReversedArray.concat(reversedArray)
+ const array = [2,4,6,8,10,12,14,16,18,20]
+let n = 3 
+console.log(NONreverseArray(array, n) +',' + reverseArray(array, n))
 
 
-const testCase1 = [2,4,6,8,10,12,16]
-const index = 3
-reversed(testCase1, index)
-nonReversed(testCase1, index)
-console.log(completeArray)
+// Output:
+// 2,4,6,20,18,16,14,12,10,8
