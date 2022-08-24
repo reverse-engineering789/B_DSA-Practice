a9aa03a6c0159e90bb19626eaa33a563fc3eb36f
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-24 11:15:51 +0530

    Lecture_022_All_About_Char_Arrays_and_Strings/022_02_01_L_B_Reverse_a_String.js

--- Lecture_022_All_About_Char_Arrays_and_Strings/022_02_01_L_B_Reverse_a_String.js
+++ Lecture_022_All_About_Char_Arrays_and_Strings/022_02_01_L_B_Reverse_a_String.js
@@ -1,37 +1,70 @@
-// Not getting any output
+// // Not getting any output
 
 
 
-let reverse = (name) => {
-    // let s = 0
-    // let e = name.length-1
+// let reverse = (name) => {
+//     // let s = 0
+//     // let e = name.length-1
 
-    // while (s < e) {
-    //     let temp = name[s]
-    //     name[s] = name[e]
-    //     name[e] = temp
-    //     name[s++]
-    //     name[e--]
-    // }
-    for (let s = 0; s <= name.length-1; s++){
-        for (let e = name.length-1; e >= 0; e--){
-            let temp = name[s]
-            name[s] = name[e]
-            name[e] = temp
+//     // while (s < e) {
+//     //     let temp = name[s]
+//     //     name[s] = name[e]
+//     //     name[e] = temp
+//     //     name[s++]
+//     //     name[e--]
+//     // }
+//     for (let s = 0; s <= name.length-1; s++){
+//         for (let e = name.length-1; e >= 0; e--){
+//             let temp = name[s]
+//             name[s] = name[e]
+//             name[e] = temp
+//         }
+//     }
+// }
+
+
+// let getLength = (arr) => {
+//     let count = 0 
+//     for (let i = 0; i <= arr.length; i++){
+//         count++
+//     }
+//     return count
+// }
+
+// reverse('Surya')
+// getLength('Surya')
+
+
+// // ------------------------------------------------
+
+
+// not giveing proper outpu
+
+const reverse = (arr) => {
+    for (let i = 0; i <= arr.length-1; i++){
+        for (let j = arr.length-1; j >= 0; j--){
+               let temp = arr[i]
+               arr[i] = arr[j]
+               arr[j] = temp
         }
     }
+    console.log(arr)
 }
 
-
-let getLength = (arr) => {
-    let count = 0 
-    for (let i = 0; i <= arr.length; i++){
-        count++
+const print = (arr) => {
+    let string = ''
+    for (let i = 0; i <= arr.length-1; i++){
+       string = string + arr[i]
     }
-    return count
+    console.log(string)
 }
 
-reverse('Surya')
-getLength('Surya')
 
 
+const name1 = 'After noon'
+reverse(name1)
+print(name1)
+
+// Output:
+// After noon
+// After noon
