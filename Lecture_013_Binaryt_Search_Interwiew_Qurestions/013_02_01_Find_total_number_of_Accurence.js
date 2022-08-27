093eac569e277fe41af95128876abca5530b324b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-27 21:54:52 +0530

    Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_02_01_Find_total_number_of_Accurence.js

--- Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_02_01_Find_total_number_of_Accurence.js
+++ Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_02_01_Find_total_number_of_Accurence.js
@@ -1,15 +1,75 @@
-const totalOcc = (arr, key) => {
-    let count = 0
-       for (let i = 1; i <= arr.length-1; i++){
-           if (arr[i] === key){
-               count++
-           }
-       } 
-       return count
+let firstOcc = (arr, key) => {
+    let s = 0;
+    let e = arr.length-1
+    let ans = -1
+    let mid = s + Math.floor(s + (e-s)/ 2)
+
+
+    while (s <= e){
+        if (arr[mid] === key){
+        //    console.log(arr[mid])
+            ans = mid
+            e = mid - 1
+
+        }else if (key > arr[mid]){
+            s = mid + 1
+            
+        }else if (key < arr[mid]){
+            e = mid - 1
+        }
+
+        mid = s + Math.floor((e-s)/ 2)
+       // console.log(mid)
+    }
+    return ans
 }
 
 
-let array = [1,2,3,3,3,3,3,3,3,3,3,3,5]
-const totalOccurece =  totalOcc(array, 5)
+let LastOcc = (arr, key) => {
+   let s = 0;
+   let e = arr.length-1
+   let ans = -1
+   let mid = s + Math.floor(s + (e-s)/ 2)
+
+
+   while (s <= e){
+       if (arr[mid] === key){
+         // console.log('arr[mid] '+arr[mid])
+           ans = mid
+           s = mid + 1
+           //console.log('s' + s)
+
+       }else if (key > arr[mid]){
+           s = mid + 1
+           
+       }else if (key < arr[mid]){
+           e = mid - 1
+       }
+       
+       mid = s + Math.floor((e-s)/ 2)
+   //   console.log(mid)
+   }
+   return ans
+}
+
+
+let even = [1,2,3,3,3,3,3,3,3,3,3,3,5]
+const firstOccurece =  firstOcc(even, 3)
+const LastOccurece =  LastOcc(even, 3)
+
+const totalOccurece = (LastOccurece -firstOccurece) + 1
 console.log(totalOccurece)
+
+
+// // Output:
+// // 10
+
+
+
+// // ---------------------------------------------
+
+
+
+
+
 
