5f1c91fbc922c47a27e2a26953aaa742104a2ad6
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-28 21:33:16 +0530

    Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_01_First_and_Last_occurence-2.js

--- Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_01_First_and_Last_occurence-2.js
+++ Lecture_013_Binaryt_Search_Interwiew_Qurestions/013_01_First_and_Last_occurence-2.js
@@ -1,31 +1,4 @@
 // let firstOcc = (arr, key) => {
-//      let s = 0;
-//      let e = arr.length-1
-//      let ans = -1
-//      let mid = s + Math.floor(s + (e-s)/ 2)
-
-
-//      while (s <= e){
-//          if (arr[mid] === key){
-//          //    console.log(arr[mid])
-//              ans = mid
-//              e = mid - 1
-
-//          }else if (key > arr[mid]){
-//              s = mid + 1
-             
-//          }else if (key < arr[mid]){
-//              e = mid - 1
-//          }
-
-//          mid = s + Math.floor((e-s)/ 2)
-//         // console.log(mid)
-//      }
-//      return ans
-// }
-
-
-// let LastOcc = (arr, key) => {
 //     let s = 0;
 //     let e = arr.length-1
 //     let ans = -1
@@ -34,82 +7,111 @@
 
 //     while (s <= e){
 //         if (arr[mid] === key){
-//           // console.log('arr[mid] '+arr[mid])
+//         //    console.log(arr[mid])
 //             ans = mid
-//             s = mid + 1
-//             //console.log('s' + s)
+//             e = mid - 1
 
 //         }else if (key > arr[mid]){
 //             s = mid + 1
-            
+
 //         }else if (key < arr[mid]){
 //             e = mid - 1
 //         }
-        
+
 //         mid = s + Math.floor((e-s)/ 2)
-//     //   console.log(mid)
+//        // console.log(mid)
 //     }
 //     return ans
 // }
 
 
+// let LastOcc = (arr, key) => {
+//    let s = 0;
+//    let e = arr.length-1
+//    let ans = -1
+//    let mid = s + Math.floor(s + (e-s)/ 2)
+
+
+//    while (s <= e){
+//        if (arr[mid] === key){
+//          // console.log('arr[mid] '+arr[mid])
+//            ans = mid
+//            s = mid + 1
+//            //console.log('s' + s)
+
+//        }else if (key > arr[mid]){
+//            s = mid + 1
+
+//        }else if (key < arr[mid]){
+//            e = mid - 1
+//        }
+
+//        mid = s + Math.floor((e-s)/ 2)
+//    //   console.log(mid)
+//    }
+//    return ans
+// }
+
+
 // let even = [1,2,3,3,3,3,3,3,3,3,3,3,5]
-// console.log('first acc of 3 is: ' + firstOcc(even, 3))
-// console.log('last acc of 3 is: ' + LastOcc(even, 3))
+// console.log(firstOcc(even, 3))
+// console.log(LastOcc(even, 3))
+// const firstOccurece =  firstOcc(even, 3)
+// const LastOccurece =  LastOcc(even, 3)
+
+// const totalOccurece = (LastOccurece -firstOccurece) + 1
+// console.log(totalOccurece)
 
 
-
-// // Output:
-// // first acc of 3 is: 2
-// // last acc of 3 is: 3
-
+// // // Output:
+// // // 2
+// // // 11
+// // // 10
 
 
 // // -----------------------------------------------------
 
-const firstOccurece = (arr,key) => {
-       let  start = 0
-       let  end = arr.length-1
-       let ans = -1
-        mid = Math.floor(start + (end - start)/2)
-           while (start <= end){
-        if (arr[mid] === key){
-            ans = mid
-            e = mid - 1
-        }
-        if (arr[mid] > key){
-            start = Mid + 1
-        }else{
+const firstOcc = (arr, key) => {
+    let start = 0
+    let end = arr.length - 1
+    let answer = -1
+    let  mid = start + Math.floor(start+(end-start)/2)
+    while (start <= end) {
+        
+        if (arr[mid] === key) {
+            answer = mid
+            end = mid - 1
+        }else if (key > arr[mid] ) {
+            start = mid + 1
+        }else if (key < arr[mid]) {
             end = mid - 1
         }
-       mid = (start + (end - start) / 2)
+       mid = start + Math.floor((end-start)/2)
     }
-    return ans 
+    return answer
 }
 
 
 const LastOcc = (arr, key) => {
-   let start = 0
-   let end = arr.length-1 
-   let answer = -2
-    let mid = (start + (end - start)/2)
-   while (start <= end){
-    if (arr[mid] === key){
-         answer = arr[mid]
-         start = mid + 1
+    let start = 0
+    let end = arr.length - 1
+    let answer = -2
+    let  mid = start + Math.floor(start+(end-start)/2)
+    while (start <= end) {
+        
+         if (arr[mid] === key) {
+            answer = mid
+            start = mid + 1
+        }else if (key > arr[mid]){
+            start = mid + 1
+        }else if (key < arr[mid]){
+            end = mid - 1
+        }
+        mid = start + Math.floor((end-start)/2)
     }
-    if (arr[mid] > key){
-        start = mid + 1
-    }else{
-        end = mid - 1
-    }
-    mid = (start + (end - start)/2)
-   }
-   return answer
+    return answer
 }
 
-
-
-let even = [1,2,3,3,3,3,3,3,3,3,3,3,5]
-console.log('first acc of 3 is: ' + firstOccurece(even, 3))
-console.log('last acc of 3 is: ' + LastOcc(even, 3))
+array = [1,2,2,2,2,3,4,5,6,8]
+console.log(firstOcc(array, 2))
+console.log(LastOcc(array, 2))
