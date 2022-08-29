14fd1ccbf0905586430b8c6c5afd164da6584014
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-29 23:36:28 +0530

    Lecture_014_Binary_Search_Interview_Questions/014_02_Serarch_In_Rotated_Sorted_Array.js

--- Lecture_014_Binary_Search_Interview_Questions/014_02_Serarch_In_Rotated_Sorted_Array.js
+++ Lecture_014_Binary_Search_Interview_Questions/014_02_Serarch_In_Rotated_Sorted_Array.js
@@ -1,61 +1,63 @@
-let getPivot = (arr) => {
-    let s = 0
-    let e = arr.length - 1
-    let mid = s + Math.floor((e - s) / 2)
+// let getPivot = (arr) => {
+//     let s = 0
+//     let e = arr.length - 1
+//     let mid = s + Math.floor((e - s) / 2)
 
 
-while (s < e) {
-    if (arr[mid] >= arr[0]){
-        s = mid + 1
-    }else{
-        e = mid
-    }
-    mid = s + Math.floor((e - s) / 2)
-}
-return s 
-}
+// while (s < e) {
+//     if (arr[mid] >= arr[0]){
+//         s = mid + 1
+//     }else{
+//         e = mid
+//     }
+//     mid = s + Math.floor((e - s) / 2)
+// }
+// return s 
+// }
 
 
-let binarySearch = (arr, s, e, key) => {
+// let binarySearch = (arr, s, e, key) => {
 
 
-    let start = s 
-    let end = e
+//     let start = s 
+//     let end = e
 
-    let mid = start + Math.floor((end - start) / 2)
+//     let mid = start + Math.floor((end - start) / 2)
 
-    while (start <= end) {
-        if (arr[mid] === key) {
-            return mid
-        }
-        if (key > arr[mid]){
-            start = mid + 1
-        }else{
-            end = mid - 1
-        }
-        mid = start + Math.floor((end - start) / 2)
-    }
-    return -1
-}
-
-
-let findPostion = (arr, k) => {
-   let pivot = getPivot(arr)
-   if(k >= arr[pivot] && k <= arr[arr.length-1]){
-        return binarySearch(arr, pivot, arr.length-1, k)
-   } else {
-       return binarySearch(arr, 0, pivot-1, k )
-   }
-}
+//     while (start <= end) {
+//         if (arr[mid] === key) {
+//             return mid
+//         }
+//         if (key > arr[mid]){
+//             start = mid + 1
+//         }else{
+//             end = mid - 1
+//         }
+//         mid = start + Math.floor((end - start) / 2)
+//     }
+//     return -1
+// }
 
 
-let test1 = [7,8,1,3,5]
+// let findPostion = (arr, k) => {
+//    let pivot = getPivot(arr)
+//    if(k >= arr[pivot] && k <= arr[arr.length-1]){
+//         return binarySearch(arr, pivot, arr.length-1, k)
+//    } else {
+//        return binarySearch(arr, 0, pivot-1, k )
+//    }
+// }
 
-let check = findPostion(test1, 5)
 
-console.log(check)
+// let test1 = [7,8,1,3,5]
 
-// Output:
-// 4
+// let check = findPostion(test1, 5)
 
+// console.log(check)
+
+// // Output:
+// // 4
+
+
+// // // ---------------------------------------------------------
 
